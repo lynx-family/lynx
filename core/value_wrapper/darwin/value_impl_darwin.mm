c6b0e25b6681faf75aa5aff61fa1a8f7dd37ca87
FrendyChen <obtenirchen@gmail.com>
2025-03-17 13:54:56 +0800

    [BugFix] Add nil protection to provent from crashing
    When nullptr's char* argument is used to construct an NSString, it returns nil. Using nil as a Dictionary key or Value will crash. Add nil protection to provent from crashing.
    
    issue: m-6123411133

--- core/value_wrapper/darwin/value_impl_darwin.mm
+++ core/value_wrapper/darwin/value_impl_darwin.mm
@@ -209,7 +209,7 @@ bool ValueImplDarwin::PushStringToArray(const std::string& value) {
   if (![backend_value_ isKindOfClass:[NSMutableArray class]]) {
     return false;
   }
-  [(NSMutableArray*)backend_value_ addObject:[NSString stringWithUTF8String:value.c_str()]];
+  [(NSMutableArray*)backend_value_ addObject:[NSString stringWithUTF8String:value.c_str()] ?: @""];
   return true;
 }
 
@@ -217,7 +217,7 @@ bool ValueImplDarwin::PushBigIntToArray(const std::string& value) {
   if (![backend_value_ isKindOfClass:[NSMutableArray class]]) {
     return false;
   }
-  [(NSMutableArray*)backend_value_ addObject:[NSString stringWithUTF8String:value.c_str()]];
+  [(NSMutableArray*)backend_value_ addObject:[NSString stringWithUTF8String:value.c_str()] ?: @""];
   return true;
 }
 
@@ -318,8 +318,9 @@ bool ValueImplDarwin::PushStringToMap(const std::string& key, const std::string&
   if (![backend_value_ isKindOfClass:[NSMutableDictionary class]]) {
     return false;
   }
-  [(NSMutableDictionary*)backend_value_ setObject:[NSString stringWithUTF8String:value.c_str()]
-                                           forKey:[NSString stringWithUTF8String:key.c_str()]];
+  [(NSMutableDictionary*)backend_value_
+      setObject:[NSString stringWithUTF8String:value.c_str()] ?: @""
+         forKey:[NSString stringWithUTF8String:key.c_str()]];
   return true;
 }
 
@@ -327,8 +328,9 @@ bool ValueImplDarwin::PushBigIntToMap(const std::string& key, const std::string&
   if (![backend_value_ isKindOfClass:[NSMutableDictionary class]]) {
     return false;
   }
-  [(NSMutableDictionary*)backend_value_ setObject:[NSString stringWithUTF8String:value.c_str()]
-                                           forKey:[NSString stringWithUTF8String:key.c_str()]];
+  [(NSMutableDictionary*)backend_value_
+      setObject:[NSString stringWithUTF8String:value.c_str()] ?: @""
+         forKey:[NSString stringWithUTF8String:key.c_str()]];
   return true;
 }
 
