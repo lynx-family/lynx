f941e195ff8abfce123a4716be6f1c52f58bae44
AdrianLCA <luochangan.adrian@gmail.com>
2025-04-22 22:14:25 +0800

    [BugFix][Gesture] Fix the crash caused by multi-threaded access to config lepusValue in parallel resolve
    Fix the crash caused by multi-threaded access to config lepusValue in parallel resolve
    
    AutoLand:release/3.2,release/3.1
    
    issue: m-6563001688
    
    AutoSubmit: True
    
    (cherry picked from commit 562033fbf5720d5417c5df01044ffceaba396f7d)
    
    (cherry picked from commit b3132dda7a2e322647b5c82f0dea13fc9e62b9be)

--- core/runtime/bindings/lepus/renderer_functions.cc
+++ core/runtime/bindings/lepus/renderer_functions.cc
@@ -412,7 +412,7 @@ GestureDetector InnerCreateGestureDetector(double gesture_id,
   BASE_STATIC_STRING_DECL(kCallbacks, "callbacks");
   BASE_STATIC_STRING_DECL(kConfig, "config");
   auto callbacks = callback_config->GetProperty(kCallbacks);
-  auto config = callback_config->GetProperty(kConfig);
+  auto config = callback_config->GetProperty(kConfig).ToLepusValue();
 
   // Initialize a vector to store gesture callbacks with their names and
   // functions.
@@ -459,11 +459,14 @@ GestureDetector InnerCreateGestureDetector(double gesture_id,
   // Extract "simultaneous", "waitFor", "continueWith" properties from the
   // "relationMap" argument.
   auto simultaneous_value =
-      relation_map_value->GetProperty(BASE_STATIC_STRING(kGestureSimultaneous));
+      relation_map_value->GetProperty(BASE_STATIC_STRING(kGestureSimultaneous))
+          .ToLepusValue();
   auto wait_for_value =
-      relation_map_value->GetProperty(BASE_STATIC_STRING(kGestureWaitFor));
+      relation_map_value->GetProperty(BASE_STATIC_STRING(kGestureWaitFor))
+          .ToLepusValue();
   auto continue_with_value =
-      relation_map_value->GetProperty(BASE_STATIC_STRING(kGestureContinueWith));
+      relation_map_value->GetProperty(BASE_STATIC_STRING(kGestureContinueWith))
+          .ToLepusValue();
 
   // Initialize vectors to store the gesture relation map (simultaneous,
   // wait_for and continue_with) values.
