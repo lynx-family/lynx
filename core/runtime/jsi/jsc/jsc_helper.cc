5d02f9e25416d4bc8c9dccab668d0f1a40116e13
ci_lynx <ci_lynx@users.noreply.github.com>
2025-05-21 02:51:37 +0000

    fix: revert "[BugFix] try fix jsc_helper call crash"
    This reverts commit 97ba2e21b768822b1d093448200050d8deb6086b

--- core/runtime/jsi/jsc/jsc_helper.cc
+++ core/runtime/jsi/jsc/jsc_helper.cc
@@ -367,10 +367,6 @@ bool JSCHelper::smellsLikeES6Symbol(JSGlobalContextRef ctx, JSValueRef ref) {
 std::optional<Value> JSCHelper::call(JSGlobalContextRef ctx, JSCRuntime& rt,
                                      const Function& f, const Value& jsThis,
                                      const Value* args, size_t nArgs) {
-  if (!ctx || !rt.Valid()) {
-    LOGE("jsc runtime has been destroyed.");
-    return std::optional<Value>();
-  }
   JSValueRef exc = nullptr;
   auto converter =
       ArgsConverter<JSValueRef>(nArgs, args, [&ctx, &rt](const auto& value) {
@@ -391,10 +387,6 @@ std::optional<Value> JSCHelper::callAsConstructor(JSGlobalContextRef ctx,
                                                   const Function& f,
                                                   const Value* args,
                                                   size_t nArgs) {
-  if (!ctx || !rt.Valid()) {
-    LOGE("jsc runtime has been destroyed.");
-    return std::optional<Value>();
-  }
   JSValueRef exc = nullptr;
   auto converter =
       ArgsConverter<JSValueRef>(nArgs, args, [&ctx, &rt](const auto& value) {
