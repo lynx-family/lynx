8e9266d4b68e3f124eed5eaff1c02b1cecad5be6
yuweizheng <2375267940@qq.com>
2025-06-26 17:53:40 +0800

    [BugFix][DevTool] add safeguard checks in RunDevToolFunction to prevent false error logs
    add safeguard checks in RunDevToolFunction to prevent false error logs
    
    issue:f-6713287610
    AutoLand: release/3.3, release/3.2
    
    (cherry picked from commit b69863448cc73ec86f6d62768cd32ebc70780cb5)

--- core/renderer/dom/fiber/fiber_element.cc
+++ core/renderer/dom/fiber/fiber_element.cc
@@ -546,8 +546,7 @@ void FiberElement::InsertedInto(FiberElement *insertion_point) {
   if (resolve_status_ == AsyncResolveStatus::kPrepareRequested) {
     AsyncPostResolveTaskToThreadPool();
   }
-  EXEC_EXPR_FOR_INSPECTOR(if (element_manager() != nullptr &&
-                              element_manager()->IsDomTreeEnabled()) {
+  EXEC_EXPR_FOR_INSPECTOR(if (element_manager() != nullptr) {
     element_manager()->RunDevToolFunction(
         lynx::devtool::DevToolFunction::InitStyleRoot, std::make_tuple(this));
   });
