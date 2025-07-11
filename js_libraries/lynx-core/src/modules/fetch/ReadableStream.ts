e8ac936eb66792127bc03695cba8fcb82a9919a9
ci_lynx <ci_lynx@users.noreply.github.com>
2025-07-11 11:23:30 +0800

    [Feature][Network] fetch API support chunck streaming
    fetch API support chunck streaming
    
    issue: m-6732104185
    
    AutoSubmit: True

---
+++ js_libraries/lynx-core/src/modules/fetch/ReadableStream.ts
@@ -0,0 +1,94 @@
+import EventEmitter from '../event';
+
+interface StreamDelegate {
+  onData(data: ArrayBuffer): void;
+  onEnd(): void;
+  onError(error: string): void;
+}
+export function createReadableStreamClass(Promise: PromiseConstructor): any {
+  return class ReadableStream implements StreamDelegate {
+    private __eventCenter: EventEmitter;
+    private __dataReceived: ArrayBuffer[];
+    private __done: boolean;
+    private __cancelled: boolean;
+    private __locked: boolean;
+    private __error: Error;
+    constructor() {
+      this.__dataReceived = [];
+      this.__done = false;
+      this.__cancelled = false;
+      this.__locked = false;
+      this.__eventCenter = new EventEmitter();
+    }
+    onData(data: ArrayBuffer): void {
+      if (this.__cancelled) {
+        return;
+      }
+      this.__dataReceived.push(data);
+      this.__eventCenter.emit('waitSignal', null);
+    }
+    onEnd(): void {
+      this.__done = true;
+      this.__eventCenter.emit('waitSignal', null);
+    }
+    onError(error: string): void {
+      this.__error = new Error(error);
+      this.__eventCenter.emit('waitSignal', null);
+    }
+    private processRead(resolve, reject) {
+      if (this.__error) {
+        return reject(this.__error);
+      }
+      if (
+        this.__cancelled ||
+        (this.__done && this.__dataReceived.length == 0)
+      ) {
+        return resolve({ done: true, value: undefined });
+      }
+      if (this.__dataReceived.length > 0) {
+        const currData = this.__dataReceived.shift();
+        return resolve({ done: false, value: currData });
+      }
+      // wait for signals
+      const waitSignal = () => {
+        this.__eventCenter.removeListener('waitSignal', waitSignal);
+        this.processRead(resolve, reject);
+      };
+
+      this.__eventCenter.addListener('waitSignal', waitSignal, this);
+    }
+    public __read() {
+      return new Promise((resolve, reject) => {
+        this.processRead(resolve, reject);
+      });
+    }
+    public get locked() {
+      return this.__locked;
+    }
+    public cancel(reason?: any) {
+      this.__cancelled = true;
+      this.__dataReceived = null;
+      this.__eventCenter.emit('waitSignal', null);
+      return Promise.resolve(reason);
+    }
+    public getReader() {
+      if (this.__locked) {
+        return null;
+      }
+      this.__locked = true;
+      return new ReadableStreamDefaultReader(this as any);
+    }
+  };
+}
+class ReadableStreamDefaultReader {
+  private __stream;
+  constructor(stream: ReadableStream) {
+    this.__stream = stream;
+  }
+  public cancel(reason?: any) {
+    return this.__stream.cancel(reason);
+  }
+  public read() {
+    return this.__stream.__read();
+  }
+}
--- platform/darwin/ios/lynx/module/LynxFetchModule.m
+++ platform/darwin/ios/lynx/module/LynxFetchModule.m
@@ -9,8 +9,21 @@
 #import <Lynx/LynxService.h>
 #import <Lynx/LynxServiceHttpProtocol.h>
 #import <objc/runtime.h>
+#import <stdatomic.h>
 
-@implementation LynxFetchModule
+@implementation LynxFetchModule {
+  LynxFetchModuleEventSender *_eventSender;
+}
+
+static atomic_long streamingCounter;
+NSString *const streamingEventNamePrefix = @"LynxFetchModuleStreamingEvent";
+
+- (instancetype)initWithParam:(id)param {
+  if (self = [super init]) {
+    _eventSender = param;
+  }
+  return self;
+}
 
 + (NSString *)name {
   return @"LynxFetchModule";
@@ -20,6 +33,48 @@
   return @{
     @"fetch" : NSStringFromSelector(@selector(fetch:resolve:reject:)),
   };
+}
+
+- (void)request:(LynxHttpRequest *)httpRequest
+        withResolve:(LynxCallbackBlock)resolve
+    withHttpService:(id<LynxServiceHttpProtocol>)httpService {
+  LynxHttpCallback block = ^(LynxHttpResponse *response) {
+    resolve(@{
+      @"url" : httpRequest.url ?: @"",
+      @"body" : response.httpBody ?: [[NSData new] init],
+      @"headers" : response.httpHeaders ?: @{},
+      @"status" : @(response.statusCode),
+      @"statusText" : response.statusText ?: @"",
+      @"lynxExtension" : response.customInfo ?: @{},
+    });
+  };
+
+  [httpService invokeWithRequest:httpRequest callback:block];
+}
+
+- (void)requestStreaming:(LynxHttpRequest *)httpRequest
+             withResolve:(LynxCallbackBlock)resolve
+         withHttpService:(id<LynxServiceHttpProtocol>)httpService {
+  NSString *streamingId = [NSString
+      stringWithFormat:@"%@%ld", streamingEventNamePrefix, atomic_fetch_add(&streamingCounter, 1)];
+  LynxHttpCallback block = ^(LynxHttpResponse *response) {
+    NSMutableDictionary *customInfo = [response.customInfo ?: @{} mutableCopy];
+    customInfo[@"streamingId"] = streamingId;
+
+    resolve(@{
+      @"url" : httpRequest.url ?: @"",
+      @"body" : [[NSData new] init],
+      @"headers" : response.httpHeaders ?: @{},
+      @"status" : @(response.statusCode),
+      @"statusText" : response.statusText ?: @"",
+      @"lynxExtension" : customInfo,
+    });
+  };
+
+  LynxHttpStreamingDelegate *delegate =
+      [[LynxHttpStreamingDelegate alloc] initWithParam:_eventSender withStreamingId:streamingId];
+
+  [httpService invokeStreamingWithRequest:httpRequest callback:block withDelegate:delegate];
 }
 
 - (void)fetch:(NSDictionary *)request
@@ -32,17 +87,7 @@
   httpRequest.httpHeaders = request[@"headers"];
   httpRequest.httpBody = request[@"body"];
   httpRequest.customConfig = request[@"lynxExtension"];
-
-  LynxHttpCallback block = ^(LynxHttpResponse *response) {
-    resolve(@{
-      @"url" : httpRequest.url ?: @"",
-      @"body" : response.httpBody ?: [[NSData new] init],
-      @"headers" : response.httpHeaders ?: @{},
-      @"status" : @(response.statusCode),
-      @"statusText" : response.statusText ?: @"",
-      @"lynxExtension" : response.customInfo ?: @{},
-    });
-  };
+  BOOL useStreaming = httpRequest.customConfig[@"useStreaming"] ?: NO;
 
   id<LynxServiceHttpProtocol> httpService = LynxService(LynxServiceHttpProtocol);
   if (!httpService) {
@@ -52,7 +97,11 @@
     return;
   }
 
-  [httpService invokeWithRequest:httpRequest callback:block];
+  if (!useStreaming) {
+    [self request:httpRequest withResolve:resolve withHttpService:httpService];
+  } else {
+    [self requestStreaming:httpRequest withResolve:resolve withHttpService:httpService];
+  }
 }
 
 @end
