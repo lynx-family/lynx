1f656920ea0e51ae22d1fc4700c204252d15c0d8
ci_lynx <ci_lynx@users.noreply.github.com>
2025-09-01 10:03:27 +0000

    fix: revert "[BugFix][Harmony] Fix background layer drawing logic to properly handle border radius rendering"
    This reverts commit 3947dddcc42109a03cfe5ba17474da1433f99525

--- platform/harmony/lynx_harmony/src/main/cpp/ui/background/background_drawable.cc
+++ platform/harmony/lynx_harmony/src/main/cpp/ui/background/background_drawable.cc
@@ -185,8 +185,7 @@ void BackgroundDrawable::DrawBackground(OH_Drawing_Canvas* canvas) {
     }
     layer_manager_->Draw(canvas, *border_box_rect_.get(),
                          *padding_box_rect_.get(), *content_box_rect_.get(),
-                         outer_path, inner_path,
-                         has_border_ || !border_radius_->IsZero());
+                         outer_path, inner_path, has_border_);
     OH_Drawing_CanvasRestore(canvas);
   }
 }
