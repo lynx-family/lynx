974fdf4b28123aaa940faf3427a25de734d7f68c
ci_lynx <16253004+pilipala195@users.noreply.github.com>
2025-09-09 17:14:03 +0800

    [BugFix] RequestFlushUIOperation in ElementManager::RequestLayout when embedded mdoe on
    In embedded mode, `ElementManager::RequestLayout` directly calls `TemplateAssembler::OnLayoutAfter`. Inside `OnLayoutAfter`, the current context is cleared, which causes a null pointer exception when `GetCurrentPipelineContext()->RequestFlushUIOperation` is executed after `ElementManager::RequestLayout` finishes.
    
    As a solution, we moved `RequestFlushUIOperation` into `ElementManager::RequestLayout` to fix the issue where it was being called after the current context was cleared.
    
    (cherry picked from commit 93f7e8300a7cbc4798c5f213ae305129270930bf)

--- core/renderer/template_assembler.cc
+++ core/renderer/template_assembler.cc
@@ -3448,7 +3448,6 @@ void TemplateAssembler::RequestLayout(
   TRACE_EVENT(LYNX_TRACE_CATEGORY, "TemplateAssembler::RequestLayout");
   if (page_proxy()->element_manager()->IsLayoutInElementModeOn()) {
     page_proxy()->element_manager()->RequestLayout(pipeline_options);
-    GetCurrentPipelineContext()->RequestFlushUIOperation();
     return;
   }
 
