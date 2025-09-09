f4d151217789185dccabaa52b370b60ed72aaf31
zhongyr <lukeren97@gmail.com>
2025-09-09 15:34:49 +0800

    [Feature][Element] Add Fragment to store layout result and styles
    In the next step of embedded pipeline, we use `Fragment` to store
    the layout result and styles, then generate displaylist as the
    render content.
    
    issue: f-6578359527

---
+++ core/renderer/dom/fragment/fragment.cc
@@ -0,0 +1,34 @@
+// Copyright 2025 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
+#include "core/renderer/dom/fragment/fragment.h"
+
+#include <utility>
+
+#include "core/renderer/css/computed_css_style.h"
+#include "core/renderer/ui_wrapper/painting/painting_context.h"
+
+namespace lynx {
+namespace tasm {
+
+Fragment::Fragment(const starlight::ComputedCSSStyle& style, int sign,
+                   PaintingContext& painting_context, base::String tag)
+    : style_{style},
+      sign_{sign},
+      painting_context_{painting_context},
+      tag_(std::move(tag)) {}
+
+void Fragment::CreateLayerIfNeeded() {
+  // TODO(zhongyr): Create HostPlatformRenderer here.
+  painting_context_.CreatePaintingNode(sign_, Tag().str(), nullptr, false,
+                                       false, 0);
+}
+
+void Fragment::UpdateLayout(
+    LayoutResultForRendering layout_result_for_rendering) {
+  layout_result_for_rendering_ = std::move(layout_result_for_rendering);
+}
+
+}  // namespace tasm
+}  // namespace lynx
---
+++ core/renderer/dom/fragment/fragment.h
@@ -0,0 +1,55 @@
+// Copyright 2025 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+#ifndef CORE_RENDERER_DOM_FRAGMENT_FRAGMENT_H_
+#define CORE_RENDERER_DOM_FRAGMENT_FRAGMENT_H_
+
+#include "core/renderer/starlight/types/layout_result.h"
+#include "core/renderer/ui_wrapper/painting/painting_context.h"
+
+namespace lynx {
+namespace starlight {
+class ComputedCSSStyle;
+}
+namespace tasm {
+
+using starlight::LayoutResultForRendering;
+
+// Combines layout results and rendering styles to generate display content
+// via DisplayList. Owned by an element; lifetime must not exceed that element.
+class Fragment : public fml::RefCountedThreadSafeStorage {
+ public:
+  Fragment(const starlight::ComputedCSSStyle& style, int sign,
+           PaintingContext& painting_context, base::String tag);
+
+  void CreateLayerIfNeeded();
+
+  void UpdateLayout(LayoutResultForRendering layout_result_for_rendering);
+
+ private:
+  virtual ~Fragment() = default;
+
+  const base::String& Tag() const { return tag_; };
+
+  base::MoveOnlyClosure<bool> should_create_layer_;
+
+  // TODO(zhongyr): children management methods.
+  base::InlineVector<fml::RefPtr<Fragment>, kChildrenInlineVectorSize>
+      children_;
+  // Sign is used to identify the fragment in the tree. Same to the sign in
+  // element.
+  const int sign_;
+
+  LayoutResultForRendering layout_result_for_rendering_;
+  PaintingContext& painting_context_;
+
+  // XXX(zhongyr): Do we need a refCounted style? Fragment's lifetime should not
+  // exceed the element who owns it.
+  const starlight::ComputedCSSStyle& style_;
+  const base::String tag_;
+};
+
+}  // namespace tasm
+}  // namespace lynx
+
+#endif  // CORE_RENDERER_DOM_FRAGMENT_FRAGMENT_H_
