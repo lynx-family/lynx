3fe9dbede081df9b400ed78b366c0be366c49c08
ci_lynx <mr.moonfacex@gmail.com>
2025-09-11 13:58:37 +0800

    [Optimize][ScrollView]BaseScrollView-part8: Add support for new scroll view architecture
    Add kScrollNewArch constant and platform_node_tag_ to enable new scroll view architecture. Update ScrollElement to handle the new attribute and override GetPlatformNodeTag.
    
    (cherry picked from commit f61d89129db67c63b5c2472bb69c2160145c446e)
    
    (cherry picked from commit 4a51646a7122bc80b823ce966a0f5f6a50c8900a)

--- core/renderer/dom/fiber/scroll_element.h
+++ core/renderer/dom/fiber/scroll_element.h
@@ -25,6 +25,10 @@ class ScrollElement : public FiberElement {
 
   bool is_scroll_view() const override { return true; }
 
+  virtual const base::String& GetPlatformNodeTag() const override {
+    return platform_node_tag_;
+  }
+
  protected:
   void OnNodeAdded(FiberElement* child) override;
   void SetAttributeInternal(const base::String& key,
@@ -35,6 +39,7 @@ class ScrollElement : public FiberElement {
 
  private:
   void HandleLayoutNodeAttributeUpdate();
+  base::String platform_node_tag_{BASE_STATIC_STRING(kElementScrollViewTag)};
 };
 
 }  // namespace tasm
