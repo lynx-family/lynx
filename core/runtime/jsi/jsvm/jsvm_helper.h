8f9ad06ca633189053857f1fa0efc35697b02dc3
ci_lynx <16253004+pilipala195@users.noreply.github.com>
2025-09-18 20:11:27 +0800

    [BugFix][Harmony] clear pending exception after JSVM call failed
    When a pending exception exists in HarmonyOS JSVM, subsequent calls will fail. We need to manually clear the exception and print relevant error logs whenever a JSVM call fails.

--- core/runtime/jsi/jsvm/jsvm_helper.h
+++ core/runtime/jsi/jsvm/jsvm_helper.h
@@ -16,37 +16,37 @@ class JSVMRuntime;
 namespace detail {
 class JSVMSymbolValue final : public Runtime::PointerValue {
  public:
-  JSVMSymbolValue(JSVM_Env env, JSVM_Value sym_val);
+  JSVMSymbolValue(JSVMRuntime* rt, JSVM_Value sym_val);
   void invalidate() override;
   std::string Name() override { return "JSVMSymbolValue"; }
 
-  JSVM_Env env_;
+  JSVMRuntime* rt_;
   JSVM_Ref sym_ref_;
 };
 
 class JSVMStringValue final : public Runtime::PointerValue {
  public:
-  JSVMStringValue(JSVM_Env env, JSVM_Value str_val);
+  JSVMStringValue(JSVMRuntime* rt, JSVM_Value str_val);
   void invalidate() override;
   std::string Name() override { return "JSVMStringValue"; }
 
-  JSVM_Env env_;
+  JSVMRuntime* rt_;
   JSVM_Ref str_ref_;
 };
 
 class JSVMObjectValue final : public Runtime::PointerValue {
  public:
-  JSVMObjectValue(JSVM_Env env, JSVM_Value obj_val);
+  JSVMObjectValue(JSVMRuntime* rt, JSVM_Value obj_val);
   void invalidate() override;
   std::string Name() override { return "JSVMObjectValue"; }
 
-  JSVM_Env env_;
+  JSVMRuntime* rt_;
   JSVM_Ref obj_ref_;
 };
 
 class JSVMHelper {
  public:
-  static piper::Value createValue(JSVM_Value value, JSVM_Env env);
+  static piper::Value createValue(JSVM_Value value, JSVMRuntime* rt);
 
   static void symbolRef(const piper::Symbol& sym, JSVM_Value* value);
 
@@ -55,21 +55,21 @@ class JSVMHelper {
 
   static void objectRef(const piper::Object& obj, JSVM_Value* value);
 
-  static std::string JSStringToSTLString(JSVM_Value s, JSVM_Env env);
+  static std::string JSStringToSTLString(JSVM_Value s, JSVMRuntime* rt);
   // Factory methods for creating String/Object
-  static piper::Symbol createSymbol(JSVM_Value sym, JSVM_Env env);
-  static piper::String createString(JSVM_Value str, JSVM_Env env);
-  static piper::PropNameID createPropNameID(JSVM_Value value, JSVM_Env env);
-  static piper::Object createObject(JSVM_Env env);
-  static piper::Object createObject(JSVM_Value obj, JSVM_Env env);
+  static piper::Symbol createSymbol(JSVM_Value sym, JSVMRuntime* rt);
+  static piper::String createString(JSVM_Value str, JSVMRuntime* rt);
+  static piper::PropNameID createPropNameID(JSVM_Value value, JSVMRuntime* rt);
+  static piper::Object createObject(JSVMRuntime* rt);
+  static piper::Object createObject(JSVM_Value obj, JSVMRuntime* rt);
 
   // Used by factory methods and clone methods
   static piper::Runtime::PointerValue* makeSymbolValue(JSVM_Value sym_val,
-                                                       JSVM_Env env);
+                                                       JSVMRuntime* rt);
   static piper::Runtime::PointerValue* makeStringValue(JSVM_Value str_val,
-                                                       JSVM_Env env);
+                                                       JSVMRuntime* rt);
   static piper::Runtime::PointerValue* makeObjectValue(JSVM_Value obj_val,
-                                                       JSVM_Env env);
+                                                       JSVMRuntime* rt);
 
   static std::optional<piper::Value> call(piper::JSVMRuntime* rt,
                                           const piper::Function& f,
@@ -81,15 +81,16 @@ class JSVMHelper {
                                                        JSVM_Value* args,
                                                        int nArgs);
 
-  static void ConvertToJSVMString(JSVM_Env env, const std::string& s,
+  static void ConvertToJSVMString(JSVMRuntime* rt, const std::string& s,
                                   JSVM_Value* value);
 
-  static void ThrowJsException(JSVM_Env env, const std::string& error_message,
+  static void ThrowJsException(JSVMRuntime* rt,
+                               const std::string& error_message,
                                const std::string& error_stack);
 
-  static void EnableInspector(JSVM_Env env, bool break_next_line);
+  static void EnableInspector(JSVMRuntime* rt, bool break_next_line);
 
-  static void CloseInspector(JSVM_Env env);
+  static void CloseInspector(JSVMRuntime* rt);
 };
 
 }  // namespace detail
--- core/runtime/jsi/jsvm/jsvm_host_function.cc
+++ core/runtime/jsi/jsvm/jsvm_host_function.cc
@@ -39,49 +39,49 @@ JSVM_Value JSVMHostFunctionProxy::createFunctionFromHostFunction(
           JSVM_Value this_value = nullptr;
           size_t argc = 1;
           JSVM_Value argv = nullptr;
-          JSVM_CALL(OH_JSVM_GetCbInfo,
-                    (env, info, &argc, &argv, &this_value, nullptr));
+          JSVM_CALL(nullptr, OH_JSVM_GetCbInfo, env, info, &argc, &argv,
+                    &this_value, nullptr);
           uint64_t ptr = 0;
           bool lossless = false;
-          JSVM_CALL(OH_JSVM_GetValueBigintUint64, (env, argv, &ptr, &lossless));
+          JSVM_CALL(nullptr, OH_JSVM_GetValueBigintUint64, env, argv, &ptr,
+                    &lossless);
 
           JSVMHostFunctionProxy* proxy =
               reinterpret_cast<JSVMHostFunctionProxy*>(ptr);
-          JSVM_CALL(OH_JSVM_Wrap,
-                    (env, this_value, reinterpret_cast<void*>(proxy),
-                     JSVMHostFunctionProxy::onFinalize, nullptr, nullptr));
-          JSVM_CALL(OH_JSVM_TypeTagObject,
-                    (env, this_value, GetHostFunctionTag()));
+          JSVM_CALL(nullptr, OH_JSVM_Wrap, env, this_value,
+                    reinterpret_cast<void*>(proxy),
+                    JSVMHostFunctionProxy::onFinalize, nullptr, nullptr);
+          JSVM_CALL(nullptr, OH_JSVM_TypeTagObject, env, this_value,
+                    GetHostFunctionTag());
           return this_value;
         },
         .data = nullptr};
     static JSVM_CallbackStruct call_as_func_callback = {
         .callback = JSVMHostFunctionProxy::FunctionCallback, .data = nullptr};
-    JSVM_CALL(
-        OH_JSVM_DefineClassWithPropertyHandler,
-        (env, "HostFunctionClazz", JSVM_AUTO_LENGTH, &ctor_callback, 0, nullptr,
-         &property_cfg, &call_as_func_callback, &function_template));
-    JSVM_CALL(OH_JSVM_CreateReference,
-              (env, function_template, 1, &function_template_ref));
+    JSVM_CALL(rt, OH_JSVM_DefineClassWithPropertyHandler, env,
+              "HostFunctionClazz", JSVM_AUTO_LENGTH, &ctor_callback, 0, nullptr,
+              &property_cfg, &call_as_func_callback, &function_template);
+    JSVM_CALL(rt, OH_JSVM_CreateReference, env, function_template, 1,
+              &function_template_ref);
     rt->SetHostFunctionTemplate(function_template_ref);
   } else {
-    JSVM_CALL(OH_JSVM_GetReferenceValue,
-              (env, function_template_ref, &function_template));
+    JSVM_CALL(rt, OH_JSVM_GetReferenceValue, env, function_template_ref,
+              &function_template);
   }
 
   JSVM_Value instance = nullptr;
   JSVMHostFunctionProxy* proxy = new JSVMHostFunctionProxy(std::move(func), rt);
   JSVM_Value ptr_value = nullptr;
-  JSVM_CALL(OH_JSVM_CreateBigintUint64,
-            (env, reinterpret_cast<uint64_t>(proxy), &ptr_value));
-  JSVM_CALL(OH_JSVM_NewInstance,
-            (env, function_template, 1, &ptr_value, &instance));
+  JSVM_CALL(rt, OH_JSVM_CreateBigintUint64, env,
+            reinterpret_cast<uint64_t>(proxy), &ptr_value);
+  JSVM_CALL(rt, OH_JSVM_NewInstance, env, function_template, 1, &ptr_value,
+            &instance);
   // Set instance's prototype to Function.prototype.
   JSVM_Value global = nullptr;
-  JSVM_CALL(OH_JSVM_GetGlobal, (env, &global));
+  JSVM_CALL(rt, OH_JSVM_GetGlobal, env, &global);
   JSVM_Value func_ctor = nullptr;
-  JSVM_CALL(OH_JSVM_GetNamedProperty, (env, global, "Function", &func_ctor));
-  JSVM_CALL(OH_JSVM_ObjectSetPrototypeOf, (env, instance, func_ctor));
+  JSVM_CALL(rt, OH_JSVM_GetNamedProperty, env, global, "Function", &func_ctor);
+  JSVM_CALL(rt, OH_JSVM_ObjectSetPrototypeOf, env, instance, func_ctor);
 
   return instance;
 }
@@ -96,11 +96,11 @@ JSVM_Value JSVMHostFunctionProxy::FunctionCallback(JSVM_Env env,
                                                    JSVM_CallbackInfo info) {
   JSVM_Value this_value = nullptr;
   size_t argc = 0;
-  JSVM_CALL(OH_JSVM_GetCbInfo,
-            (env, info, &argc, nullptr, &this_value, nullptr));
+  JSVM_CALL(nullptr, OH_JSVM_GetCbInfo, env, info, &argc, nullptr, &this_value,
+            nullptr);
   JSVMHostFunctionProxy* proxy_ptr = nullptr;
-  JSVM_CALL(OH_JSVM_Unwrap,
-            (env, this_value, reinterpret_cast<void**>(&proxy_ptr)));
+  JSVM_CALL(nullptr, OH_JSVM_Unwrap, env, this_value,
+            reinterpret_cast<void**>(&proxy_ptr));
   JSVMRuntime* rt = nullptr;
   std::shared_ptr<piper::HostFunctionType> lock_host_func;
   if (proxy_ptr == nullptr ||
@@ -110,20 +110,20 @@ JSVM_Value JSVMHostFunctionProxy::FunctionCallback(JSVM_Env env,
   }
 
   std::vector<JSVM_Value> args(argc);
-  JSVM_CALL(OH_JSVM_GetCbInfo,
-            (env, info, &argc, args.data(), nullptr, nullptr));
+  JSVM_CALL(rt, OH_JSVM_GetCbInfo, env, info, &argc, args.data(), nullptr,
+            nullptr);
 
   auto converter =
-      ArgsConverter<Value>(argc, args.data(), [&env](const JSVM_Value& value) {
-        return JSVMHelper::createValue(value, env);
+      ArgsConverter<Value>(argc, args.data(), [rt](const JSVM_Value& value) {
+        return JSVMHelper::createValue(value, rt);
       });
   JSINativeExceptionCollector::Scope scope;
-  auto ret = (*lock_host_func)(*rt, JSVMHelper::createValue(this_value, env),
+  auto ret = (*lock_host_func)(*rt, JSVMHelper::createValue(this_value, rt),
                                converter, argc);
   const auto& exception =
       JSINativeExceptionCollector::Instance()->GetException();
   if (exception && rt->IsEnableJsBindingApiThrowException()) {
-    JSVMHelper::ThrowJsException(env, exception->message(), exception->stack());
+    JSVMHelper::ThrowJsException(rt, exception->message(), exception->stack());
     return nullptr;
   }
 
@@ -134,7 +134,7 @@ JSVM_Value JSVMHostFunctionProxy::FunctionCallback(JSVM_Env env,
     // TODO(huzhanbo.luc): we can merge this usage into
     // JSINativeExceptionCollector
     if (rt->IsEnableJsBindingApiThrowException()) {
-      JSVMHelper::ThrowJsException(env, ret.error().message(),
+      JSVMHelper::ThrowJsException(rt, ret.error().message(),
                                    ret.error().stack());
     } else {
       rt->reportJSIException(ret.error());
