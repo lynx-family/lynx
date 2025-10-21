f7ea5c9e0dc660f1f8c0e5dc12ab0882048dc1db
yongjieTang <1297144429@qq.com>
2025-10-21 11:46:49 +0800

    [BugFix][iOS]Fix the Lynx module compilation failure issue.
    Background:
    The Lynx module failed to compile because LynxBaseTraceService{.h, .mm} files depend on LynxServiceAPI component APIs, but the Lynx module does not have a dependency on the LynxServiceAPI component. Since LynxBaseTraceService is an independent service capability, it should be placed in the Lynx Service component for dependent parties to optionally import.
    
    Changes:
    Migrate LynxBaseTraceService{.h, .mm} to LynxService and expose them as a BaseService subspec. Also add BaseTrace dependency in explorer's podfile.
    
    AutoSubmit:true
    AutoLand: release/3.5

--- platform/darwin/ios/lynx_service/base_trace/LynxBaseTraceService.mm
+++ platform/darwin/ios/lynx_service/base_trace/LynxBaseTraceService.mm
@@ -2,7 +2,7 @@
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
 
-#import <Lynx/LynxBaseTraceService.h>
+#import <LynxService/LynxBaseTraceService.h>
 #include "base/include/base_trace/trace_event_utils.h"
 #include "base/trace/native/trace_event.h"
 
