537ef0d6bd73a7e8a6c374aa5ba10f6ef5ce9755
rAY-ooo <309262406@qq.com>
2025-11-03 18:17:02 +0800

    [BugFix][Event] Fix a gesture conflict issue when Lynx cards overlapped.
    description:
    Previously, because `OH_ArkUI_GestureEvent_GetNode` was not supported, setting a `HitTestBehavior` of type `Transparent` would reset the `event_dispatcher` to that of the bottom-level Lynx card when multiple Lynx cards overlapped. This caused gesture comparison to fail, resulting in upper-level Lynx cards failing to respond to clicks.
    
    Therefore, we set the `EventDispatcher` to `user_data` on the gesture-bound node, and then retrieved the `EventDispatcher` from the node in the custom gesture determination interface to ensure consistency.
    
    issue: m-6789472525
    
    AutoLand: release/3.5

--- platform/harmony/lynx_harmony/src/main/cpp/ui/base/node_manager.h
+++ platform/harmony/lynx_harmony/src/main/cpp/ui/base/node_manager.h
@@ -142,11 +142,12 @@ class NodeManager {
 
   void DisposeGesture(ArkUI_GestureRecognizer* recognizer);
 
-  void SetEventDispatcher(EventDispatcher* dispatcher) {
-    event_dispatcher_ = dispatcher;
-  }
+  ArkUI_GestureRecognizerType GetGestureType(
+      ArkUI_GestureRecognizer* recognizer);
 
-  EventDispatcher* GetEventDispatcher() { return event_dispatcher_; }
+  void SetUserData(ArkUI_NodeHandle node, void* user_data);
+
+  void* GetUserData(ArkUI_NodeHandle node);
 
   template <typename... Args>
   void SetAttributeWithNumberValue(ArkUI_NodeHandle node,
@@ -257,7 +258,6 @@ class NodeManager {
 
   ArkUI_NativeNodeAPI_1* native_node_api_{nullptr};
   ArkUI_NativeGestureAPI_1* native_gesture_api_{nullptr};
-  EventDispatcher* event_dispatcher_{nullptr};
 };
 }  // namespace harmony
 }  // namespace tasm
