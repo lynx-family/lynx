1edc5658fe05f20e1a617c6cd6209687e671cd39
nhsprite <1032274844@qq.com>
2025-11-03 16:39:21 +0800

    [Optimize] using LynxViewGroup to optimize Image AutoSize
    image with autosize should trigger layout reflow when loadsuccess, which is performance cost.
    Using lynxViewGroup to cache bitmapSize, so that image may accquires the bitmapSize before loadSuccess in
    cache cases, making the measure in `AutoSizeImage` return the proper size immediately.
    
    issue: f-6784758001
    AutoSubmit: true
    AutoLand: release/3.5

--- platform/android/lynx_android/src/main/java/com/lynx/tasm/LynxTemplateRender.java
+++ platform/android/lynx_android/src/main/java/com/lynx/tasm/LynxTemplateRender.java
@@ -138,6 +138,7 @@ public class LynxTemplateRender
   private TemplateBundle mTemplateBundle;
   private ILynxViewConfigProvider mLynxViewConfigProvider;
   private ILynxViewGroup mLynxViewGroup;
+  private ILynxViewRuntimeCacheManager mCacheManager;
   private ILynxLogicExecutor mLogicExecutor;
 
   private LynxBackgroundRuntimeOptions mLynxRuntimeOptions;
@@ -301,6 +302,9 @@ public class LynxTemplateRender
     mLynxViewConfigProvider = builder;
     mLogicExecutor = builder.getLogicExecutor();
     mLynxViewGroup = builder.lynxViewGroup;
+    if (builder.lynxViewGroup instanceof ILynxViewRuntimeCacheManager) {
+      mCacheManager = (ILynxViewRuntimeCacheManager) builder.lynxViewGroup;
+    }
 
     mLynxViewBuilder = builder;
     if (mLynxViewGroup != null) {
@@ -406,6 +410,7 @@ public class LynxTemplateRender
             onErrorOccurred(error);
           }
         };
+    mLynxContext.setRuntimeCacheManager(this.mCacheManager);
     mTemplateAssembler.setLynxContext(mLynxContext);
 
     mLynxContext.setEmbeddedMode(mEmbeddedMode);
