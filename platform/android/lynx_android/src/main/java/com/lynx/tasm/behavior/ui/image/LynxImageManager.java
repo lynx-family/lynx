a421f41c13899f7c335bd50f4638b8163d5a382e
liubb0516 <106364053+liubb0516@users.noreply.github.com>
2025-11-06 11:15:06 +0800

    [BugFix] Fix Lynx image getUrl crash
    Added null checks for `mCurPlaceholderRequest` to prevent `getUrl` crashes.
    
    m-6791533583

--- platform/android/lynx_android/src/main/java/com/lynx/tasm/behavior/ui/image/LynxImageManager.java
+++ platform/android/lynx_android/src/main/java/com/lynx/tasm/behavior/ui/image/LynxImageManager.java
@@ -368,7 +368,8 @@ public class LynxImageManager implements Drawable.Callback {
     @Override
     public void onSuccess(
         @Nullable ImageContent imageContent, ImageRequestInfo requestInfo, ImageInfo imageInfo) {
-      if (!TextUtils.equals(requestInfo.getUrl(), mCurPlaceholderRequest.getUrl())) {
+      if (!TextUtils.equals(requestInfo.getUrl(),
+              mCurPlaceholderRequest != null ? mCurPlaceholderRequest.getUrl() : null)) {
         return;
       }
       mPlaceholderDrawable = new LynxScaleTypeDrawable(imageContent, mMode);
