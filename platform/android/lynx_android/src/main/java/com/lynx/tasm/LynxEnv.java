dbace761c3a27f0099adeeef76cb3ed756750bb3
linxs0211 <linxs0211@gmail.com>
2025-11-07 01:24:15 +0800

    [BugFix][Image] revert enableImageRequestOptimize settings as false
    set enableImageRequestOptimize as false by default
    
    issue:m-6792118552
    
    AutoSubmit:true

--- platform/android/lynx_android/src/main/java/com/lynx/tasm/LynxEnv.java
+++ platform/android/lynx_android/src/main/java/com/lynx/tasm/LynxEnv.java
@@ -1284,7 +1284,7 @@ public class LynxEnv {
     mEnableImageAsyncLayout =
         getBooleanFromExternalEnv(LynxEnvKey.ENABLE_IMAGE_ASYNC_LAYOUT, false);
     mEnableImageRequestOptimize =
-        getBooleanFromExternalEnv(LynxEnvKey.ENABLE_IMAGE_REQUEST_OPTIMIZE, true);
+        getBooleanFromExternalEnv(LynxEnvKey.ENABLE_IMAGE_REQUEST_OPTIMIZE, false);
     mEnableFlattenImageFlickerFix =
         getBooleanFromExternalEnv(LynxEnvKey.ENABLE_FLATTEN_IMAGE_FLICKER_FIX, true);
   }
