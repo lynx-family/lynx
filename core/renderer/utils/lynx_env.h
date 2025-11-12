49e8202069386b7079c764b3d77c83e50de6bfa9
nhsprite <1032274844@qq.com>
2025-11-12 20:52:32 +0800

    [BugFix][Reland] It's not allowed for platform_data to be modified.
    1. there may be case that value_converted_from_platform_data_ is already exist before post to js, make a shallowCopy to make it safe.
    2. when `GenerateTemplateDataPostedToJs` to generate a templateData to be consumed by js thread, we just need to copy the updateActions but now the raw lepus::Value is taken to js thread either, it may
    causing the lepus::Value freed on the js thread
    
    this relands mr: !58616
    
    issue: f-6792636224
    AutoSubmit: true

--- core/renderer/utils/lynx_env.h
+++ core/renderer/utils/lynx_env.h
@@ -123,6 +123,7 @@ class LYNX_EXPORT_FOR_DEVTOOL LynxEnv {
     ENABLE_GLOBAL_FONT_COLLECTION,
     ENABLE_GC_ONCE_ON_IDLE,
     DISABLE_JS_MODE_STRIP,
+    ENABLE_PLATFORM_DATA_FIX,
     ENABLE_QUICKJS_THREAD_CHECKER,
     LYNX_DEBUG_ENABLED,
     ENABLE_JS_CALLBACK_MANAGER,
@@ -264,6 +265,7 @@ class LYNX_EXPORT_FOR_DEVTOOL LynxEnv {
              "enable_global_font_collection"},
             {Key::ENABLE_GC_ONCE_ON_IDLE, "enable_gc_once_on_idle"},
             {Key::DISABLE_JS_MODE_STRIP, "disable_js_mode_strip"},
+            {Key::ENABLE_PLATFORM_DATA_FIX, "enable_platform_data_fix"},
             {Key::ENABLE_QUICKJS_THREAD_CHECKER,
              "enable_quickjs_thread_checker"},
             {Key::ENABLE_JS_CALLBACK_MANAGER, "enable_js_callback_manager"},
@@ -409,6 +411,7 @@ class LYNX_EXPORT_FOR_DEVTOOL LynxEnv {
   bool EnableGlobalFontCollection();
   uint32_t EnableGCOnceOnIdle();
   bool DisableJSModeStrip();
+  bool EnablePlatformDataFix();
   bool EnableQuickJsThreadChecker();
 
   LynxEnv(const LynxEnv&) = delete;
