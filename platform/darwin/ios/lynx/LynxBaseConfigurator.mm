9d28c6b46582148583349a6f24e69d5b501d57a3
nhsprite <1032274844@qq.com>
2025-11-24 21:41:55 +0800

    [BugFix] Considering LynxThreadStrategy configged by LynxViewBuilder.
    It's reasonable that LynxThreadStrategy set by LynxViewBuilder is with higher priority to LynxViewGroup, fix it in this patch.
    1. add a `_hasThreadStragySet` flag that identifies the threadStrategy has been set manually.
    2. when calling `getThreadStrategyForRendering`, if `_hasThreadStragySet` is true, we should return the strategy set by lynxViewBuilder
    else return with the strategy set by lynxViewGroup
    
    issue: f-6792923644
    AutoSubmit: true

--- platform/darwin/ios/lynx/LynxBaseConfigurator.mm
+++ platform/darwin/ios/lynx/LynxBaseConfigurator.mm
@@ -2,7 +2,7 @@
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
 
-#import "Lynx/LynxBaseConfigurator.h"
+#import <Lynx/LynxBaseConfigurator.h>
 #import <Lynx/LynxFontFaceManager.h>
 #import <Lynx/LynxLazyRegister.h>
 #import <Lynx/LynxLog.h>
@@ -13,7 +13,6 @@
 #import "LynxUIRendererCreator.h"
 
 @implementation LynxBaseConfigurator {
-  LynxThreadStrategyForRender _threadStrategy;
   LynxEmbeddedMode _embeddedMode;
   NSMutableDictionary<NSString*, LynxAliasFontInfo*>* _builderRegisteredAliasFontMap;
 }
@@ -32,6 +31,8 @@
     self.uiRendererCreator = [[LynxUIRendererCreator alloc] init];
     self.lynxBackgroundRuntimeOptions = [[LynxBackgroundRuntimeOptions alloc] init];
     _builderRegisteredAliasFontMap = [NSMutableDictionary dictionary];
+    _threadStrategy = LynxThreadStrategyForRenderAllOnUI;
+    _hasThreadStrategySet = NO;
   }
   return self;
 }
@@ -52,6 +53,7 @@
 }
 
 - (void)setThreadStrategyForRender:(LynxThreadStrategyForRender)threadStrategy {
+  _hasThreadStrategySet = YES;
   switch (threadStrategy) {
     case LynxThreadStrategyForRenderAllOnUI:
     case LynxThreadStrategyForRenderPartOnLayout:
