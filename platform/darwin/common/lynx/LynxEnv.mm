3c53188b9f4e69884b429eafb67332cbb2773721
Dango-2021 <191667655+Dango-2021@users.noreply.github.com>
2025-11-25 21:00:14 +0800

    [BugFix][DevTool] Fix BTS debugging-related logics still executing after turning off `lynx_debug_enabled` switch.
    In C++, use `lynx_debug_enabled && enable_devtool` to determine if devtool is enabled.
    
    AutoLand: release/3.5

--- platform/darwin/common/lynx/LynxEnv.mm
+++ platform/darwin/common/lynx/LynxEnv.mm
@@ -136,6 +136,8 @@
 
 - (void)setLynxDebugEnabled:(BOOL)lynxDebugEnabled {
   [LynxService(LynxServiceDevToolProtocol) setLynxDebugPresetValue:lynxDebugEnabled];
+  lynx::tasm::LynxEnv::GetInstance().SetBoolLocalEnv([KEY_LYNX_DEBUG UTF8String],
+                                                     [self lynxDebugEnabled]);
   [self initDevToolEnv];
 }
 
