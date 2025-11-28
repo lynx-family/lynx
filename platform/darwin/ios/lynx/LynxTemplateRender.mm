0e54fca6a1aafe776ac683e92106b99b7f33e667
nhsprite <1032274844@qq.com>
2025-11-28 17:46:42 +0800

    [BugFix] fix iOS sendGlobalEvent in Embedded.
    fix iOS sendGlobalEvent in Embedded
    
    issue: f-8723656
    AutoSubmit: true

--- platform/darwin/ios/lynx/LynxTemplateRender.mm
+++ platform/darwin/ios/lynx/LynxTemplateRender.mm
@@ -1217,6 +1217,14 @@ LYNX_NOT_IMPLEMENTED(-(instancetype)initWithCoder : (NSCoder*)aDecoder)
   } else {
     _LogE(@"TemplateRender %p sendGlobalEvent %@ error, can't get LynxContext", self, name);
   }
+
+  if (_context && _context.isEmbeddedModeOn) {
+    [self onLynxEventWithDictionary:@{
+      @"method" : kSendGlobalEvent,
+      @"name" : name,
+      @"params" : params
+    }];
+  }
 }
 
 - (void)sendGlobalEventToLepus:(nonnull NSString*)name withParams:(nullable NSArray*)params {
