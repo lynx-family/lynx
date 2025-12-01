cefbea2d27063f34a4de797e20da30f025a653f6
ci_lynx <ci_lynx@users.noreply.github.com>
2025-12-01 06:33:03 +0000

    Revert "[BugFix][Event] Fix a crash issue caused by an error in retrieving UserData in Node."
    This reverts commit 07d84246

--- platform/harmony/lynx_harmony/src/main/cpp/ui/base/node_manager.h
+++ platform/harmony/lynx_harmony/src/main/cpp/ui/base/node_manager.h
@@ -132,7 +132,7 @@ class NodeManager {
                              void (*target_receiver)(ArkUI_GestureEvent* event,
                                                      void* extra_params));
 
-  void SetGestureInterrupterToNode(ArkUI_NodeHandle node, void* user_data,
+  void SetGestureInterrupterToNode(ArkUI_NodeHandle node,
                                    ArkUI_GestureInterruptResult (*interrupter)(
                                        ArkUI_GestureInterruptInfo* info));
 
@@ -257,7 +257,6 @@ class NodeManager {
 
   ArkUI_NativeNodeAPI_1* native_node_api_{nullptr};
   ArkUI_NativeGestureAPI_1* native_gesture_api_{nullptr};
-  void* native_gesture_api_2{nullptr};
   EventDispatcher* event_dispatcher_{nullptr};
 };
 }  // namespace harmony
