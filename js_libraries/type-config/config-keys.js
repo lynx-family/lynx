9727c34473e30a64fd35d475b744e707d8c7336d
rAY-ooo <309262406@qq.com>
2026-01-06 19:40:43 +0800

    [Testing] CQ-CQ-705374-60662-enqgv

--- js_libraries/type-config/config-keys.js
+++ js_libraries/type-config/config-keys.js
@@ -102,6 +102,7 @@ const configKeys = [
   'enableTextOverflow',
   'enableTextRefactor',
   'enableTouchRefactor',
+  'enableTransformedTouchPosition',
   'enableUIOperationOptimize',
   'enableUnifiedPipeline',
   'enableUseContextPool',
