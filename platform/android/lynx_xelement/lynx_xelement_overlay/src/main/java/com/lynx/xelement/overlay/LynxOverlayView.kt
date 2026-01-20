bbc45350b05121af57cd8b2fdfdb6e3a9ee37268
AdrianLCA <luochangan.adrian@gmail.com>
2026-01-20 17:42:19 +0800

    [Optimize][Android][Overlay] Add android-adapt-edge-to-edge property
    1. Add `android-adapt-edge-to-edge` property for LynxOverlay to support edge-
      to-edge display.
    2. Specifically handle Android 15 (API 35) edge-to-edge requirements using
      `WindowCompat.setDecorFitsSystemWindows`.
    3. Add `androidx.core:core` dependency to support `WindowCompat` utility.
    
    AutoSubmit: True
    
    AutoLand: release/3.6

--- platform/android/lynx_xelement/lynx_xelement_overlay/src/main/java/com/lynx/xelement/overlay/LynxOverlayView.kt
+++ platform/android/lynx_xelement/lynx_xelement_overlay/src/main/java/com/lynx/xelement/overlay/LynxOverlayView.kt
@@ -12,6 +12,7 @@ import android.graphics.Rect
 import android.os.Build
 import android.view.*
 import androidx.annotation.RequiresApi
+import androidx.core.view.WindowCompat
 import com.lynx.react.bridge.Dynamic
 import com.lynx.react.bridge.ReadableType
 import com.lynx.tasm.LynxError
@@ -105,6 +106,8 @@ class LynxOverlayView(context: LynxContext, val proxy: LynxUIOverlay) : UIGroup<
                 false
             }
         }
+        // By default, it remains consistent with not enabling edge to edge.
+        setAdaptEdgeToEdge(false)
         mOverlayContainer.isClickable = true
         mOverlayContainer.isFocusable = true
         mOverlayContainer.isFocusableInTouchMode = true
@@ -434,6 +437,15 @@ class LynxOverlayView(context: LynxContext, val proxy: LynxUIOverlay) : UIGroup<
         mDialog.containerPopupTag = tag
     }
 
+    @LynxProp(name = LynxUIOverlay.PROP_ADAPT_EDGE_TO_EDGE)
+    fun setAdaptEdgeToEdge(adapt: Boolean) {
+      window?.let {
+        //Build.VERSION_CODES.VANILLA_ICE_CREAM
+        if (Build.VERSION.SDK_INT >= 35)
+          WindowCompat.setDecorFitsSystemWindows(it, !adapt)
+      }
+    }
+  
     /**
      * @name: android-set-soft-input-mode
      * @description: Set the soft keyboard mode to control the input state after the keyboard pops up [nothing | pan | resize | unspecified]
