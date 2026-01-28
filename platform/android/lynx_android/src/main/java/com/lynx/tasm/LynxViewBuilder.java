6842711cfc286b0e7a3c92ea1b3b66b39a28628a
nhsprite <1032274844@qq.com>
2026-01-28 21:24:43 +0800

    [BugFix] fix priority of threadStrategy in LynxViewBuilder
    we should consider threadStrategy configged in LynxViewBuilder as higher priority.
    
    issue: f-90823765
    AutoSubmit: true

--- platform/android/lynx_android/src/main/java/com/lynx/tasm/LynxViewBuilder.java
+++ platform/android/lynx_android/src/main/java/com/lynx/tasm/LynxViewBuilder.java
@@ -382,10 +382,13 @@ public class LynxViewBuilder
 
   @Override
   public ThreadStrategyForRendering getThreadStrategy() {
+    if (this.threadStrategy != null) {
+      return this.threadStrategy;
+    }
     if (lynxViewGroup != null) {
       return lynxViewGroup.getThreadStrategy();
     }
-    return this.threadStrategy;
+    return ThreadStrategyForRendering.ALL_ON_UI;
   }
 
   @Override
@@ -406,10 +409,13 @@ public class LynxViewBuilder
 
   @Override
   public HashMap getContextData() {
+    if (this.mContextData != null) {
+      return this.mContextData;
+    }
     if (lynxViewGroup != null) {
       return lynxViewGroup.getContextData();
     }
-    return this.mContextData;
+    return null;
   }
 
   @Override
@@ -553,6 +559,9 @@ public class LynxViewBuilder
 
   @Override
   public float getFontScale() {
+    if (hasFontScaleSet) {
+      return this.fontScale;
+    }
     if (lynxViewGroup != null) {
       return lynxViewGroup.getFontScale();
     }
