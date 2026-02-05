fd449efeb2b45716253d1036c60ced5d55a541d4
f0rdream <littlepigwang@outlook.com>
2026-02-05 11:26:56 +0800

    Revert "[BugFix] use id instead of function for timer api"
    This reverts commit 0ac8ee57fac7ab26d91979e4d160e2d58e73334b.
    This reverts commit dbfec7d9e946d8f17afaac4042494ed614172be0.
    
    AutoSubmit: True

--- js_libraries/lynx-core/src/app/interface.ts
+++ js_libraries/lynx-core/src/app/interface.ts
@@ -110,12 +110,6 @@ export type LifeEvent =
   | 'datasetChanged'
   | 'instanceChanged';
 
-//The two types are for legacy compatibility; once the experiment results are valid, only the id type will be retained.
-export type LynxSetTimeout2 = (
-  idOrCallback: number | ((...args: unknown[]) => unknown),
-  number: number
-) => number;
-
 /**
  * NativeApp is the interface that native app implements
  * implementations are in `js_app.cc`
@@ -148,9 +142,9 @@ export interface NativeApp {
 
   setCard(app: BaseApp): void;
 
-  setTimeout: LynxSetTimeout2;
+  setTimeout: LynxSetTimeout;
 
-  setInterval: LynxSetTimeout2;
+  setInterval: LynxSetTimeout;
 
   clearTimeout(timeoutId: number): void;
 
