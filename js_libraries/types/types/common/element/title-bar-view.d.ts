101b8868c3afdfe89cc803fd95776a38390ea865
ci_lynx <mr.moonfacex@gmail.com>
2026-02-25 18:14:40 +0800

    [Optimize] Standardize platform tag naming convention
    Update platform-specific tags to use consistent PascalCase format (@ClayMacOS, @ClayWindows) and add missing tag definitions in tsdoc.json. Also update corresponding documentation in title-bar-view.d.ts to reflect the new naming.

--- js_libraries/types/types/common/element/title-bar-view.d.ts
+++ js_libraries/types/types/common/element/title-bar-view.d.ts
@@ -7,15 +7,15 @@ import { StandardProps } from '../props';
 /**
  * The title-bar-view element is similar to the app-region style in the web,
  * allowing developers to customize the window dragging area.
- * @clay_windows
- * @clay_macOS
+ * @ClayWindows
+ * @ClayMacOS
  */
 export interface TitleBarViewProps extends StandardProps {
   /**
    * When set to true, the title bar view is able to move the window when dragged.
    * @defaultValue false
-   * @clay_windows
-   * @clay_macOS
+   * @ClayWindows
+   * @ClayMacOS
    */
   'moveable'?: boolean;
 }
