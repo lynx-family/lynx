09483f6f4ca422dbfc7c162476a66a54006f57d5
Huxpro <ci_lynx@users.noreply.github.com>
2026-02-28 22:10:51 -0800

    [Feature][Explorer] Integrate Sparkling SDK into LynxExplorer
    SkipChecks:macro
    AutoSubmit: true
    
    Change-Id: I720020fd7b3ecaa3f2b85e2137b0dc5e0e375ad5

--- explorer/darwin/ios/lynx_explorer/LynxExplorer/input/LynxExplorerInput.m
+++ explorer/darwin/ios/lynx_explorer/LynxExplorer/input/LynxExplorerInput.m
@@ -62,7 +62,7 @@ LYNX_PROP_SETTER("text-color", setTextColor, NSString *) {
   UIColor *textColor = [UIHelper colorWithHexString:value];
   self.view.textColor = textColor;
 
-  UIColor *placeholderColor = [textColor colorWithAlphaComponent:0.25];
+  UIColor *placeholderColor = [textColor colorWithAlphaComponent:0.4];
   if (@available(iOS 13.0, *)) {
     NSAttributedString *attributedPlaceholder = [[NSAttributedString alloc]
         initWithString:self.view.placeholder ?: @""
