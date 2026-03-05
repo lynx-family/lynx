15e351ca62b5d941f3da6d59d3d38333b844ab9f
rAY-ooo <309262406@qq.com>
2026-03-05 17:41:29 +0800

    [Optimize][Event][Part1] NativeUIRender supports the exposure capability.
    description:
    Implement the functionality to bundle event attributes and event names for recording event information in the DisplayList. This includes:
    - Adding interfaces for event attribute name mapping, event name mapping, and event attribute value processing in `platform_event_bundle.h`.
    - Adding `event_props` and `event_names` to the Fragment, and calling the interfaces in `Element/FiberElement` to record incrementally updated event attributes and event names.
    - Adding an `EventBundle` directive to `DisplayListBuilder`, and processing the input of this directive in `DisplayList`.
    
    issue: m-6578359527

--- core/renderer/dom/fragment/display_list.h
+++ core/renderer/dom/fragment/display_list.h
@@ -10,6 +10,8 @@
 
 #include "base/include/auto_create_optional.h"
 #include "base/include/vector.h"
+#include "core/renderer/dom/fragment/event/platform_event_bundle.h"
+#include "core/value_wrapper/value_impl_lepus.h"
 
 namespace lynx {
 namespace tasm {
@@ -20,6 +22,9 @@ struct OpData {
   base::InlineVector<int32_t, 16> int_data;
   base::InlineVector<float, 16> float_data;
 };
+
+using PlatformEventPropMap =
+    base::InlineOrderedFlatMap<PlatformEventPropName, lepus::Value, 12>;
 
 enum class DisplayListOpType : int32_t {
   kBegin = 0,
@@ -33,6 +38,7 @@ enum class DisplayListOpType : int32_t {
   kClipRect = 10,
   kRecordBox = 11,
   kLinearGradient = 12,
+  kEventBundle = 13,
 };
 
 enum class DisplayListSubtreePropertyOpType : int32_t {
@@ -171,6 +177,9 @@ class DisplayList {
   void AddLinearGradient(float angle, const base::Vector<uint32_t>& colors,
                          const base::Vector<float>& stops, int32_t tiling_index,
                          int32_t clip_index);
+
+  void AddEventBundle(const PlatformEventPropMap& event_props,
+                      const base::Vector<PlatformEventName>& event_names);
 
   template <typename OpType, typename... Args>
   auto AddOperation(OpType type, Args... args) -> std::enable_if_t<
