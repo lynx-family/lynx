72f1aa86c499f94b243f4f6bac78f86484781c9b
zys777 <1498906716@qq.com>
2026-03-18 13:08:41 +0800

    [AutoRoll] Roll revisions automatically from repositories: lynx/primjs
    AutoSubmit: True
    
    FullCheck: True
    
    ''

--- devtool/js_inspect/lepus/lepus_internal/lepusng/lepusng_debugger.cc
+++ devtool/js_inspect/lepus/lepus_internal/lepusng/lepusng_debugger.cc
@@ -202,7 +202,6 @@ void LepusNGDebugger::ParseDebugInfo(const LEPUSValue& top_level_function,
       if (has_function_info) {
         FillFunctionBytecodeDebugInfo(ctx, b, debug_info_entry);
       }
-      LEPUS_WriteBarrierNoStore(ctx, script);
       SetFunctionScript(b, script);
     }
   }
