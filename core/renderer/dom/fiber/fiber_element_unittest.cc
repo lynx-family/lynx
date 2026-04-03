b1f973381e13a58a49580fc0c3c104bf922c52d1
usczz <9398044+usczz@users.noreply.github.com>
2026-04-03 12:13:03 +0800

    [BugFix][Element] Disable animation forward dynamic update overwrite fix by default
    Disable fix_animation_forward_dynamic_update_overwrite_ setting by default to avoid logic break.
    
    issue:m-7034427203
    
    AutoSubmit:true

--- core/renderer/dom/fiber/fiber_element_unittest.cc
+++ core/renderer/dom/fiber/fiber_element_unittest.cc
@@ -6267,6 +6267,7 @@ TEST_P(FiberElementTest,
 
 TEST_P(FiberElementTest, TestFiberNewAnimatorWithDynamicStyleUpdate) {
   CSSParserConfigs configs;
+  manager->fix_animation_forward_dynamic_update_overwrite_ = true;
 
   auto page = manager->CreateFiberPage("page", 11);
   auto element = manager->CreateFiberView();
