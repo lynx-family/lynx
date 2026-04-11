8919c39149fb5b6b26b06d423ea47be59a6bfd3a
gaohanqing <9686525+Kingatnuaa0528@users.noreply.github.com>
2026-04-11 21:35:42 +0800

    Revert "[BugFix] Fix memory layout crash across dll issue"
    This reverts commit 14e74b3f6a1d4dd89930ae19a6924a22b12175d2.

--- third_party/napi/weak_napi_host_generator.cc
+++ third_party/napi/weak_napi_host_generator.cc
@@ -231,8 +231,7 @@ const NodeApiRawPtrHost& GetWeakNapiRawPtrHost() {
     host->napi_get_value_bigint_words_rawptr =
         reinterpret_cast<void*>(napi_get_value_bigint_words);
     host->napi_get_version_rawptr = reinterpret_cast<void*>(napi_get_version);
-    host->napi_find_module_rawptr =
-        reinterpret_cast<void*>(napi_find_module_primjs);
+    host->napi_find_module_rawptr = reinterpret_cast<void*>(napi_find_module);
     return host;
   }();
   return *sWeakRawPtrHost;
