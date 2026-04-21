0eefd892ffef6279b1f243b0e1fc92ef652d3343
ShouruiSong <shouruisong@foxmail.com>
2026-04-22 00:12:40 +0800

    [Feature][Template] Add template element shell files
    - Add placeholder TemplateElement source and header files for the upcoming Element Template implementation.
    
    - Add the generated elements result header as a dedicated extension point for generated template payloads.
    
    - Register the new files in the fiber GN source list without adding runtime behavior yet.
    
    TEST: git diff --check
    
    AutoSubmit:True

---
+++ core/renderer/dom/fiber/generated_elements_result.h
@@ -0,0 +1,16 @@
+// Copyright 2026 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
+#ifndef CORE_RENDERER_DOM_FIBER_GENERATED_ELEMENTS_RESULT_H_
+#define CORE_RENDERER_DOM_FIBER_GENERATED_ELEMENTS_RESULT_H_
+
+namespace lynx {
+namespace tasm {
+
+// TODO(songshourui.null): Add generated element result data structures.
+
+}  // namespace tasm
+}  // namespace lynx
+
+#endif  // CORE_RENDERER_DOM_FIBER_GENERATED_ELEMENTS_RESULT_H_
---
+++ core/renderer/dom/fiber/template_element.h
@@ -0,0 +1,16 @@
+// Copyright 2026 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
+#ifndef CORE_RENDERER_DOM_FIBER_TEMPLATE_ELEMENT_H_
+#define CORE_RENDERER_DOM_FIBER_TEMPLATE_ELEMENT_H_
+
+namespace lynx {
+namespace tasm {
+
+// TODO(songshourui.null): Add TemplateElement declaration.
+
+}  // namespace tasm
+}  // namespace lynx
+
+#endif  // CORE_RENDERER_DOM_FIBER_TEMPLATE_ELEMENT_H_
