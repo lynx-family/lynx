c6a910aa9abf9fb076e81c2f55f9a25a94317dce
ci_lynx <16253004+pilipala195@users.noreply.github.com>
2026-05-07 14:37:25 +0800

    [Feature][Frame] Add frame load metrics typings
    - Add frame preset sizing and multi async thread props to the public types package.
    
    - Restrict frame preset sizing props to px/rpx unit strings and cover accepted and rejected values in type tests.
    
    - Add frame load metrics callback typings with load bundle performance entry details.
    
    - Cover the new frame props and load metrics callback in type tests, and update the 3.9.0 changelog.
    
    TEST: pnpm --dir lynx/js_libraries/types test
    
    SkipChecks: js-types-check

--- js_libraries/types/test/pages/frame.test-d.tsx
+++ js_libraries/types/test/pages/frame.test-d.tsx
@@ -3,7 +3,7 @@
 // LICENSE file in the root directory of this source tree.
 
 import { assertType, it } from 'vitest';
-import { IntrinsicElements, FrameLoadEvent } from '../../types';
+import { IntrinsicElements, FrameLoadEvent, FrameLoadMetricsEvent } from '../../types';
 
 // Props Types Check
 let a;
@@ -59,6 +59,40 @@ let a;
   <frame src={'1'} auto-width={1} />;
   // @ts-expect-error: auto-width shoudl be boolean not string
   <frame src={'1'} auto-width={'1'} />;
+
+  <frame src={'1'} preset-width={'100px'} />;
+  <frame src={'1'} preset-width={'100rpx'} />;
+  assertType<`${number}px` | `${number}rpx` | undefined>((a as unknown) as IntrinsicElements['frame']['preset-width']);
+  <frame src={'1'} preset-height={'100px'} />;
+  <frame src={'1'} preset-height={'100rpx'} />;
+  assertType<`${number}px` | `${number}rpx` | undefined>((a as unknown) as IntrinsicElements['frame']['preset-height']);
+  // @ts-expect-error: preset-width shoudl be string not number
+  <frame src={'1'} preset-width={100} />;
+  // @ts-expect-error: preset-width shoudl be string not boolean
+  <frame src={'1'} preset-width={true} />;
+  // @ts-expect-error: preset-width shoudl use px or rpx unit
+  <frame src={'1'} preset-width={'100'} />;
+  // @ts-expect-error: preset-width shoudl use px or rpx unit
+  <frame src={'1'} preset-width={'100ppx'} />;
+  // @ts-expect-error: preset-height shoudl be string not number
+  <frame src={'1'} preset-height={100} />;
+  // @ts-expect-error: preset-height shoudl be string not boolean
+  <frame src={'1'} preset-height={true} />;
+  // @ts-expect-error: preset-height shoudl use px or rpx unit
+  <frame src={'1'} preset-height={'100'} />;
+  // @ts-expect-error: preset-height shoudl use px or rpx unit
+  <frame src={'1'} preset-height={'100ppx'} />;
+
+  <frame src={'1'} enable-multi-async-thread={true} />;
+  <frame src={'1'} enable-multi-async-thread={false} />;
+  assertType<boolean | undefined>((a as unknown) as IntrinsicElements['frame']['enable-multi-async-thread']);
+  // @ts-expect-error: enable-multi-async-thread shoudl be boolean not number
+  <frame src={'1'} enable-multi-async-thread={1} />;
+  // @ts-expect-error: enable-multi-async-thread shoudl be boolean not string
+  <frame src={'1'} enable-multi-async-thread={'true'} />;
+
+  type HasEmbeddedMode = 'embedded-mode' extends keyof IntrinsicElements['frame'] ? true : false;
+  assertType<false>((a as unknown) as HasEmbeddedMode);
 }
 
 // Events types check
@@ -70,6 +104,18 @@ function noop() {}
       assertType<string>(e.detail.url);
       assertType<number>(e.detail.statusCode);
       assertType<string>(e.detail.statusMessage);
+    }}
+  />;
+  <frame
+    src={'1'}
+    bindloadmetrics={(e: FrameLoadMetricsEvent) => {
+      assertType<'bindloadmetrics'>(e.type);
+      assertType<string>(e.detail.url);
+      assertType<string>(e.detail.mode);
+      assertType<string | undefined>(e.detail.entry.name);
+      assertType<string | undefined>(e.detail.entry.entryType);
+      assertType<number | undefined>(e.detail.entry.loadBundleStart);
+      assertType<unknown>(e.detail.entry.customMetric);
     }}
   />;
 }
