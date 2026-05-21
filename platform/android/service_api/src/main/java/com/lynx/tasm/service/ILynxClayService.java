e5bd0d44040965c557e2c8b59683cf5e4cc6c19b
ci_lynx <ci_lynx@users.noreply.github.com>
2026-05-21 17:40:07 +0800

    [Optimize] Add Clay service preinitialization
    Add clay service to preload clay classes

---
+++ platform/android/service_api/src/main/java/com/lynx/tasm/service/ILynxClayService.java
@@ -0,0 +1,19 @@
+// Copyright 2026 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
+package com.lynx.tasm.service;
+
+import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
+
+@Keep
+public interface ILynxClayService extends IServiceProvider {
+  /**
+   * Get service class, DO NOT OVERRIDE THIS METHOD
+   */
+  @NonNull
+  default Class<? extends IServiceProvider> getServiceClass() {
+    return ILynxClayService.class;
+  }
+}
