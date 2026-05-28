eacdb5e847782041f352672af6cf9a2bb12ec337
Kingatnuaa0528 <1277082377@qq.com>
2026-05-28 10:59:51 +0800

    [Infra] Add retry time for ios-e2e-core-test
    As above.
    
    issue: #5312999715

--- tools/rtf/core/utils/gn_args_helper.py
+++ tools/rtf/core/utils/gn_args_helper.py
@@ -3,9 +3,9 @@
 # LICENSE file in the root directory of this source tree.
 
 
-def inject_flutter_cxx(builders, disable_flutter_cxx: bool):
+def inject_flutter_cxx(builders, enable_flutter_cxx: bool):
     """Inject or update use_flutter_cxx in GN builder args."""
-    flutter_cxx_value = "false" if disable_flutter_cxx else "true"
+    flutter_cxx_value = "true" if enable_flutter_cxx else "false"
     for builder in builders.values():
         args_list = builder.setdefault("args", [])
         for i, arg in enumerate(args_list):
