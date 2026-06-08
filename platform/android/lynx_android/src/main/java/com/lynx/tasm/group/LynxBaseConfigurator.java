1b9d7725d29740566e76fcb68f02c5979070b7b7
nhsprite <1032274844@qq.com>
2026-06-08 20:52:46 +0800

    [BugFix] Preserve explicit Android builder overrides
    - Track whether LynxBaseConfigurator values were explicitly set instead of inferring that from default field values.
    - Make LynxViewBuilder getters prefer explicit builder overrides before inheriting from LynxViewGroup so false and override values are preserved.
    - This fixes Android configuration precedence bugs when callers intentionally disable features or override runtime, density, screen, and behavior settings.
    
    issue: f-97236546
    AutoSubmit: true

--- platform/android/lynx_android/src/main/java/com/lynx/tasm/group/LynxBaseConfigurator.java
+++ platform/android/lynx_android/src/main/java/com/lynx/tasm/group/LynxBaseConfigurator.java
@@ -29,16 +29,25 @@ import java.util.List;
 
 public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
   protected boolean enableMultiAsyncThread = true;
+  protected boolean hasEnableMultiAsyncThreadSet = false;
   protected Float densityOverride;
+  protected boolean hasDensitySet = false;
   protected BehaviorRegistry behaviorRegistry;
+  protected boolean hasBehaviorRegistrySet = false;
   protected LynxBackgroundRuntimeOptions lynxRuntimeOptions;
   protected boolean enableAutoExpose;
+  protected boolean hasEnableAutoExposeSet = false;
   protected boolean enableLayoutSafepoint;
+  protected boolean hasEnableLayoutSafepointSet = false;
   protected boolean enableUnifiedPipeline;
+  protected boolean hasEnableUnifiedPipelineSet = false;
   protected boolean forceDarkAllowed = false;
+  protected boolean hasForceDarkAllowedSet = false;
   protected boolean enableSyncFlush = false;
+  protected boolean hasEnableSyncFlushSet = false;
   @Deprecated protected boolean enableAutoConcurrency = false;
   protected boolean enableVSyncAlignedMessageLoop = false;
+  protected boolean hasEnableVSyncAlignedMessageLoopSet = false;
   protected boolean enablePendingJsTask = false;
   protected boolean hasPendingJsTaskSet = false;
   protected boolean hasPresetMeasureSpec = false;
@@ -49,6 +58,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    * enable async hydration of ssr.
    */
   protected boolean enableAsyncHydration = false;
+  protected boolean hasEnableAsyncHydrationSet = false;
 
   /**
    * enableJSRuntime、enableAirStrictMode both determine whether js thread will be enabled.
@@ -56,6 +66,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    * usage.
    */
   protected boolean enableJSRuntime = true;
+  protected boolean hasEnableJSRuntimeSet = false;
 
   /**
    * Add switch for Air Mode.
@@ -63,26 +74,34 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    * and further optimized logics may be involved.
    */
   protected boolean enableAirStrictMode = false;
+  protected boolean hasEnableAirStrictModeSet = false;
   protected boolean debuggable = false;
+  protected boolean hasDebuggableSet = false;
   protected int presetWidthMeasureSpec;
   protected int presetHeightMeasureSpec;
   protected float fontScale = 1.0f;
   protected boolean hasFontScaleSet = false;
 
   protected boolean enablePreUpdateData = false;
+  protected boolean hasEnablePreUpdateDataSet = false;
   protected HashMap<String, Object> mContextData;
 
   protected ThreadStrategyForRendering threadStrategy = null;
 
   protected int screenWidth = DisplayMetricsHolder.UNDEFINE_SCREEN_SIZE_VALUE;
   protected int screenHeight = DisplayMetricsHolder.UNDEFINE_SCREEN_SIZE_VALUE;
+  protected boolean hasScreenSizeSet = false;
 
   protected IUIRendererCreator uiRendererCreator;
+  protected boolean hasUIRendererCreatorSet = false;
 
   protected int embeddedMode = EmbeddedMode.UNSET;
+  protected boolean hasEmbeddedModeSet = false;
   protected boolean enableMTSModule = false;
+  protected boolean hasEnableMTSModuleSet = false;
 
   protected String tapSlop = TouchEventDispatcher.mTapSlopDefault;
+  protected boolean hasTapSlopSet = false;
 
   public LynxBaseConfigurator() {
     LynxEnv.inst().lazyInitIfNeeded();
@@ -111,6 +130,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public void setCustomBehaviorRegistry(@NonNull BehaviorRegistry registry) {
     this.behaviorRegistry = registry;
+    this.hasBehaviorRegistrySet = true;
   }
 
   /**
@@ -126,6 +146,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
   public T setScreenSize(int width, int height) {
     this.screenWidth = width;
     this.screenHeight = height;
+    this.hasScreenSizeSet = true;
     return (T) this;
   }
 
@@ -136,6 +157,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
   public T setBehaviors(@Nullable List<Behavior> bundle) {
     if (bundle != null) {
       behaviorRegistry.addBehaviors(bundle);
+      hasBehaviorRegistrySet = true;
     }
     return (T) this;
   }
@@ -145,6 +167,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T addBehaviors(@NonNull List<Behavior> behaviorList) {
     this.behaviorRegistry.addBehaviors(behaviorList);
+    this.hasBehaviorRegistrySet = true;
     return (T) this;
   }
 
@@ -153,6 +176,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T addBehavior(@NonNull Behavior behavior) {
     this.behaviorRegistry.addBehavior(behavior);
+    this.hasBehaviorRegistrySet = true;
     return (T) this;
   }
 
@@ -163,6 +187,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T enableAutoExpose(boolean enableAutoExpose) {
     this.enableAutoExpose = enableAutoExpose;
+    this.hasEnableAutoExposeSet = true;
     return (T) this;
   }
 
@@ -222,6 +247,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setDensity(float density) {
     this.densityOverride = density;
+    this.hasDensitySet = true;
     return (T) this;
   }
 
@@ -280,6 +306,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setEnableLayoutSafepoint(boolean enable) {
     enableLayoutSafepoint = enable;
+    hasEnableLayoutSafepointSet = true;
     return (T) this;
   }
 
@@ -291,6 +318,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setEnableUnifiedPipeline(boolean enableUnifiedPipeline) {
     this.enableUnifiedPipeline = enableUnifiedPipeline;
+    this.hasEnableUnifiedPipelineSet = true;
     return (T) this;
   }
 
@@ -336,6 +364,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
 
   public T setForceDarkAllowed(boolean allowed) {
     this.forceDarkAllowed = allowed;
+    this.hasForceDarkAllowedSet = true;
     return (T) this;
   }
 
@@ -346,6 +375,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setEnableMultiAsyncThread(boolean enableMultiAsyncThread) {
     this.enableMultiAsyncThread = enableMultiAsyncThread;
+    this.hasEnableMultiAsyncThreadSet = true;
     return (T) this;
   }
 
@@ -364,6 +394,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setEnableSyncFlush(boolean enable) {
     enableSyncFlush = enable;
+    hasEnableSyncFlushSet = true;
     return (T) this;
   }
 
@@ -401,6 +432,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setEnableVSyncAlignedMessageLoop(boolean enable) {
     enableVSyncAlignedMessageLoop = enable;
+    hasEnableVSyncAlignedMessageLoopSet = true;
     return (T) this;
   }
 
@@ -409,16 +441,19 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setEnableAsyncHydration(boolean enable) {
     enableAsyncHydration = enable;
+    hasEnableAsyncHydrationSet = true;
     return (T) this;
   }
 
   public T setEnableJSRuntime(boolean enable) {
     enableJSRuntime = enable;
+    hasEnableJSRuntimeSet = true;
     return (T) this;
   }
 
   public T setEnableAirStrictMode(boolean enable) {
     enableAirStrictMode = enable;
+    hasEnableAirStrictModeSet = true;
     return (T) this;
   }
 
@@ -429,6 +464,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setDebuggable(boolean enable) {
     debuggable = enable;
+    hasDebuggableSet = true;
     return (T) this;
   }
 
@@ -462,11 +498,13 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setEnablePreUpdateData(boolean enable) {
     enablePreUpdateData = enable;
+    hasEnablePreUpdateDataSet = true;
     return (T) this;
   }
 
   public T setUIRendererCreator(@NonNull IUIRendererCreator uiRendererCreator) {
     this.uiRendererCreator = uiRendererCreator;
+    this.hasUIRendererCreatorSet = true;
     return (T) this;
   }
 
@@ -482,6 +520,7 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
    */
   public T setEmbeddedMode(@EmbeddedMode.Mode int embeddedMode) {
     this.embeddedMode = embeddedMode;
+    this.hasEmbeddedModeSet = true;
     if ((embeddedMode & EmbeddedMode.EMBEDDED_MODE_BASE) > 0) {
       behaviorRegistry.setBuiltInBehaviors(BuiltInUIRegistry.getInstance().getBuiltInUIBehaviors());
     }
@@ -495,11 +534,13 @@ public class LynxBaseConfigurator<T extends LynxBaseConfigurator<T>> {
 
   public T setEnableMTSModule(boolean enable) {
     enableMTSModule = enable;
+    hasEnableMTSModuleSet = true;
     return (T) this;
   }
 
   public T setTapSlop(String tapSlop) {
     this.tapSlop = tapSlop;
+    this.hasTapSlopSet = true;
     return (T) this;
   }
 }
