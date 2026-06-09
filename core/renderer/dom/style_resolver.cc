0055074224e2342b19100da8fe967206c50dff3a
ci_lynx <ci_lynx@users.noreply.github.com>
2026-06-09 11:14:19 +0800

    [BugFix] Mark mask composite and -x-caret extension changes in ComputeStyleDiff
    Style diffing missed several properties, causing patching to skip updates
    when these values changed.
    
    Key changes:
    - Include 'mask-image' composite ('kPropertyIDMaskComposite') in background/
      mask diff logic when present;
    - Mark caret extension fields as changed during diffing:
      '-x-caret_gradient/width/height/radius';
    - Add unit tests to validate change marking for mask composite and caret
      extension properties.
    
    AutoLand: release/4.0
    
    issue: m-7333036248

--- core/renderer/dom/style_resolver.cc
+++ core/renderer/dom/style_resolver.cc
@@ -2387,6 +2387,7 @@ struct BackgroundDiffIds {
   CSSPropertyID size;
   CSSPropertyID origin;
   CSSPropertyID clip;
+  CSSPropertyID composite;
 };
 
 starlight::OutLineData DefaultOutline(
@@ -2536,6 +2537,10 @@ bool StyleResolver::ComputeStyleDiff(
   changed |= MarkIfChanged(new_style, n.field, o.field, ids.field);
     BACKGROUND_IMAGE_FIELDS(MARK_IMAGE_FIELD)
 #undef MARK_IMAGE_FIELD
+    if (ids.composite != kPropertyStart) {
+      changed |=
+          MarkIfChanged(new_style, n.composite, o.composite, ids.composite);
+    }
     return changed;
   };
   auto diff_background = [&](const auto& n, const auto& o,
@@ -2565,7 +2570,8 @@ bool StyleResolver::ComputeStyleDiff(
             n, o,
             {kPropertyIDBackgroundImage, kPropertyIDBackgroundPosition,
              kPropertyIDBackgroundRepeat, kPropertyIDBackgroundSize,
-             kPropertyIDBackgroundOrigin, kPropertyIDBackgroundClip},
+             kPropertyIDBackgroundOrigin, kPropertyIDBackgroundClip,
+             kPropertyStart},
             true);
       });
   changed |= DiffOptionalValue(
@@ -2573,11 +2579,12 @@ bool StyleResolver::ComputeStyleDiff(
       SharedDefaultProvider<starlight::BackgroundData>{},
       SharedDefaultProvider<starlight::BackgroundData>{},
       [&](const auto& n, const auto& o) {
-        return diff_background(n, o,
-                               {kPropertyIDMaskImage, kPropertyIDMaskPosition,
-                                kPropertyIDMaskRepeat, kPropertyIDMaskSize,
-                                kPropertyIDMaskOrigin, kPropertyIDMaskClip},
-                               false);
+        return diff_background(
+            n, o,
+            {kPropertyIDMaskImage, kPropertyIDMaskPosition,
+             kPropertyIDMaskRepeat, kPropertyIDMaskSize, kPropertyIDMaskOrigin,
+             kPropertyIDMaskClip, kPropertyIDMaskComposite},
+            false);
       });
   changed |= DiffOptionalValue(
       new_style.outline_, old_style.outline_,
@@ -2636,6 +2643,15 @@ bool StyleResolver::ComputeStyleDiff(
 
   changed |= MarkIfChanged(new_style, new_style.caret_color_,
                            old_style.caret_color_, kPropertyIDCaretColor);
+  changed |=
+      MarkIfChanged(new_style, new_style.caret_gradient_,
+                    old_style.caret_gradient_, kPropertyIDXCaretGradient);
+  changed |= MarkIfChanged(new_style, new_style.caret_width_,
+                           old_style.caret_width_, kPropertyIDXCaretWidth);
+  changed |= MarkIfChanged(new_style, new_style.caret_height_,
+                           old_style.caret_height_, kPropertyIDXCaretHeight);
+  changed |= MarkIfChanged(new_style, new_style.caret_radius_,
+                           old_style.caret_radius_, kPropertyIDXCaretRadius);
   changed |= DiffOptionalValue(
       new_style.text_attributes_, old_style.text_attributes_,
       [&new_style]() { return DefaultTextAttributes(new_style); },
