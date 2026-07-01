adfc9ad16bf28539f0eeebac4697760d8dafd3e6
nhsprite <1032274844@qq.com>
2026-07-01 17:14:29 +0800

    [BugFix] Add iOS LynxEmbeddedModule Registion
    - Added setDataV2 and getDataV2 to LynxEmbeddedModule, using the current LynxView from the module context.
    - Registered LynxEmbeddedModule by default for LynxViewGroup when no logic executor is configured.
    - Added a TemplateRender unit test to cover LynxView consuming the module registered through LynxViewGroup.
    
    issue: f-972563455
    AutoSubmit: true

--- platform/darwin/ios/lynx/module/LynxEmbeddedModule.mm
+++ platform/darwin/ios/lynx/module/LynxEmbeddedModule.mm
@@ -1,10 +1,12 @@
 // Copyright 2025 The Lynx Authors. All rights reserved.
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
+#import <Lynx/LynxContext+Internal.h>
 #import <Lynx/LynxEmbeddedModule.h>
+#import <Lynx/LynxEnv+Internal.h>
 #import <Lynx/LynxViewGroup.h>
-#import "LynxContext+Internal.h"
-#import "LynxEnv+Internal.h"
+#import <Lynx/LynxWeakProxy.h>
+#import <objc/runtime.h>
 
 @implementation LynxEmbeddedModule {
   __weak LynxContext *context_;
@@ -19,6 +21,8 @@
   return @{
     @"updateData" : NSStringFromSelector(@selector(updateData:params:resolve:reject:)),
     @"getData" : NSStringFromSelector(@selector(getData:resolve:reject:)),
+    @"setDataV2" : NSStringFromSelector(@selector(setDataV2:resolve:reject:)),
+    @"getDataV2" : NSStringFromSelector(@selector(getDataV2:reject:)),
   };
 }
 
@@ -35,7 +39,9 @@
   if (self) {
     context_ = context;
     if ([param isKindOfClass:[LynxViewGroup class]]) {
-      lynxViewGroup_ = (LynxViewGroup *)param;
+      lynxViewGroup_ = (LynxViewGroup *)(object_getClass(param) == [LynxWeakProxy class]
+                                             ? ((LynxWeakProxy *)param).target
+                                             : param);
     }
   }
   return self;
@@ -80,5 +86,56 @@
     LynxTemplateData *data = [lynxView getTemplateData];
     resolve(data);
   });
+}
+
+- (void)setDataV2:(NSDictionary *)params
+          resolve:(LynxCallbackBlock)resolve
+           reject:(LynxCallbackBlock)reject {
+  // V2 embedded APIs are callback-only. JS callers must pass resolve/reject callbacks.
+  LynxView *lynxView = [context_ getLynxView];
+  if (lynxView == nil) {
+    if (reject) {
+      reject(@{@"message" : @"Cannot get related lynxView."});
+    }
+    return;
+  }
+
+  dispatch_block_t block = ^{
+    LynxTemplateData *data = [[LynxTemplateData alloc] initWithDictionary:params];
+    LynxUpdateMeta *updateMeta = [[LynxUpdateMeta alloc] init];
+    [updateMeta setData:data];
+    [lynxView updateMetaData:updateMeta];
+    if (resolve) {
+      resolve(@{});
+    }
+  };
+  if ([NSThread isMainThread]) {
+    block();
+  } else {
+    dispatch_async(dispatch_get_main_queue(), block);
+  }
+}
+
+- (void)getDataV2:(LynxCallbackBlock)resolve reject:(LynxCallbackBlock)reject {
+  // V2 embedded APIs are callback-only. JS callers must pass resolve/reject callbacks.
+  LynxView *lynxView = [context_ getLynxView];
+  if (lynxView == nil) {
+    if (reject) {
+      reject(@{@"message" : @"Cannot get related lynxView."});
+    }
+    return;
+  }
+
+  dispatch_block_t block = ^{
+    LynxTemplateData *data = [lynxView getTemplateData];
+    if (resolve) {
+      resolve(data);
+    }
+  };
+  if ([NSThread isMainThread]) {
+    block();
+  } else {
+    dispatch_async(dispatch_get_main_queue(), block);
+  }
 }
 @end
