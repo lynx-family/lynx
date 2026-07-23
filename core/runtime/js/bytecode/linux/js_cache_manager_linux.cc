a9833e50ad6fac7ef3f4c89d40d83830896fc060
FrendyChen <obtenirchen@gmail.com>
2026-07-23 19:52:20 +0800

    [Optimize] Add Linux platform compilation support
    Adapt Linux desktop builds by wiring Linux-specific sources into shared Lynx
    targets and reducing duplicated headless platform source ownership.

---
+++ core/runtime/js/bytecode/linux/js_cache_manager_linux.cc
@@ -0,0 +1,17 @@
+// Copyright 2025 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
+#include "core/runtime/js/bytecode/js_cache_manager.h"
+
+namespace lynx {
+namespace runtime {
+namespace js {
+namespace cache {
+
+std::string JsCacheManager::GetPlatformCacheDir() { return "./"; }
+
+}  // namespace cache
+}  // namespace js
+}  // namespace runtime
+}  // namespace lynx
