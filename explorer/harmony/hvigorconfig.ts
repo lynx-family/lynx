613f7221fc7790475ab867c2dacaa840d2f192c1
jianliang00 <wangjianliang0@126.com>
2026-07-28 19:17:34 +0800

    [Feature][Harmony] Support global library Autolink

---
+++ explorer/harmony/hvigorconfig.ts
@@ -0,0 +1,10 @@
+// Copyright 2026 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
+import * as hvigorApi from '@ohos/hvigor';
+import { enableHarmonyLynxAutolink } from '@lynx/lynx-library-plugin';
+
+enableHarmonyLynxAutolink(hvigorApi, {
+  moduleName: 'lynx_explorer',
+});
