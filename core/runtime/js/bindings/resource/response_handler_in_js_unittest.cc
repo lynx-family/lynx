c788382f689143010945771ced060ac35a4d72fb
ci_lynx <46469310+RobinWuu@users.noreply.github.com>
2026-08-10 15:04:47 +0800

    [BugFix] Dispatch resource callbacks before accessing App
    - Dispatch response promise callbacks to the JS thread before locking the App weak pointer.
    - Avoid cross-thread access when resource promises resolve on loader threads.
    - Add coverage for queued callback dispatch and callback thread affinity.
    
    issue: f-7350207575
    AutoSubmit: true
    NoLanding: develop

--- core/runtime/js/bindings/resource/response_handler_in_js_unittest.cc
+++ core/runtime/js/bindings/resource/response_handler_in_js_unittest.cc
@@ -5,6 +5,8 @@
 #include "core/runtime/js/bindings/resource/response_handler_in_js.h"
 
 #include <memory>
+#include <mutex>
+#include <thread>
 
 #include "core/runtime/common/bindings/resource/response_promise.h"
 #include "core/runtime/js/bindings/js_app.h"
@@ -47,6 +49,34 @@ class TestDelegate : public runtime::test::MockTemplateDelegate {
   bool called_ = false;
 };
 
+class QueuedTestDelegate : public runtime::test::MockTemplateDelegate {
+ public:
+  void InvokeResponsePromiseCallback(base::closure closure) override {
+    std::lock_guard<std::mutex> lock(mutex_);
+    pending_callback_ = std::move(closure);
+  }
+
+  bool HasPendingCallback() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return pending_callback_ != nullptr;
+  }
+
+  void RunPendingCallback() {
+    base::closure callback;
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      callback = std::move(pending_callback_);
+    }
+    if (callback) {
+      callback();
+    }
+  }
+
+ private:
+  std::mutex mutex_;
+  base::closure pending_callback_;
+};
+
 TEST_P(ResponseHandlerInJSTest, ThenCallbackFiresWithoutHoldingHandler) {
   TestDelegate delegate;
   auto promise =
@@ -75,6 +105,46 @@ TEST_P(ResponseHandlerInJSTest, ThenCallbackFiresWithoutHoldingHandler) {
   promise->SetValue(info);
 
   EXPECT_TRUE(resource_callback_called);
+}
+
+TEST_P(ResponseHandlerInJSTest, PromiseResolutionDispatchesBeforeAccessingApp) {
+  QueuedTestDelegate delegate;
+  auto promise =
+      std::make_shared<runtime::ResponsePromise<tasm::BundleResourceInfo>>();
+
+  Object nativeModuleProxy = Object::createFromHostObject(*runtime, nullptr);
+
+  auto app = App::Create(0, runtime.GetWeakPtr(), &delegate, nullptr,
+                         std::move(nativeModuleProxy), nullptr, "-1",
+                         tasm::PageOptions());
+  const auto owner_thread = std::this_thread::get_id();
+  std::thread::id callback_thread;
+  bool resource_callback_called = false;
+  auto handler = std::make_shared<ResponseHandlerInJS>(
+      delegate,
+      /*url=*/"test-url", promise, app->GetWeakPtr());
+
+  handler->AddResourceListener([&callback_thread, &resource_callback_called](
+                                   tasm::BundleResourceInfo info) {
+    callback_thread = std::this_thread::get_id();
+    resource_callback_called = true;
+  });
+
+  std::thread resource_thread([promise]() {
+    tasm::BundleResourceInfo info;
+    info.url = "test-url";
+    info.code = 200;
+    promise->SetValue(info);
+  });
+  resource_thread.join();
+
+  EXPECT_FALSE(resource_callback_called);
+  EXPECT_TRUE(delegate.HasPendingCallback());
+
+  delegate.RunPendingCallback();
+
+  EXPECT_TRUE(resource_callback_called);
+  EXPECT_EQ(callback_thread, owner_thread);
 }
 
 INSTANTIATE_TEST_SUITE_P(
