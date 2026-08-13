9daa099a631e37fb8c4371d9e643e3bf94350ae5
zhongyr <lukeren97@gmail.com>
2026-08-13 16:39:23 +0800

    [BugFix][Android] Use density for basic shapes
    - Use Lynx density instead of scaledDensity when converting clip-path and offset-path geometry.
    - Align BasicShape APIs and Android tests with the geometry density semantics.
    - Verified with :LynxAndroid:compileNoasanDebugAndroidTestSources.

--- platform/android/lynx_android/src/main/java/com/lynx/tasm/behavior/ui/LynxUI.java
+++ platform/android/lynx_android/src/main/java/com/lynx/tasm/behavior/ui/LynxUI.java
@@ -1493,8 +1493,7 @@ public abstract class LynxUI<T extends View> extends LynxBaseUI implements IProc
 
   @LynxProp(name = "clip-path")
   public void setClipPath(@Nullable ReadableArray basicShape) {
-    mClipPath =
-        BasicShape.CreateFromReadableArray(basicShape, mContext.getScreenMetrics().scaledDensity);
+    mClipPath = BasicShape.CreateFromReadableArray(basicShape, mContext.getScreenMetrics().density);
   }
 
   @LynxProp(name = PropsConstants.OFFSET_PATH)
@@ -1502,7 +1501,7 @@ public abstract class LynxUI<T extends View> extends LynxBaseUI implements IProc
     if (mRawOffsetShape != basicShape) {
       mRawOffsetShape = basicShape;
       mOffsetPath =
-          BasicShape.CreateFromReadableArray(basicShape, mContext.getScreenMetrics().scaledDensity);
+          BasicShape.CreateFromReadableArray(basicShape, mContext.getScreenMetrics().density);
       mOffsetHasChanged = true;
     }
   }
