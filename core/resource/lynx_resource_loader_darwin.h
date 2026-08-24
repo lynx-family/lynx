7add58d8343ebe02da84533035986912dd7e3903
nhsprite <1032274844@qq.com>
2026-08-24 16:08:55 +0800

    Revert "[BugFix] Load Darwin lazy bundle fetches off the caller thread"
    This reverts commit 71f8882a89de7529c9132a3c1e3c6e1711f82fc7.

--- core/resource/lynx_resource_loader_darwin.h
+++ core/resource/lynx_resource_loader_darwin.h
@@ -45,7 +45,6 @@ class LynxResourceLoaderDarwin : public pub::LynxResourceLoader {
  private:
   using CopyableClosure =
       fml::internal::CopyableLambda<base::MoveOnlyClosure<void, pub::LynxResourceResponse&>>;
-  void LoadResourceInternalSync(const pub::LynxResourceRequest& request, CopyableClosure callback);
 
   NSData* LoadJSSource(const std::string& name);
   NSData* LoadLynxJSAsset(const std::string& name, NSURL& bundleUrl, NSURL& debugBundleUrl);
@@ -80,7 +79,8 @@ class LynxResourceLoaderDarwin : public pub::LynxResourceLoader {
    * Try to fetch template by FetcherWrapper, it will use builtin resourceService or registered
    * dynamicComponentFetcher
    */
-  bool FetchTemplateByFetcherWrapper(const std::string& url, CopyableClosure callback);
+  bool FetchTemplateByFetcherWrapper(const std::string& url, CopyableClosure callback,
+                                     bool request_in_current_thread);
 
   static void FetchExternalResourceComplete(NSData* data, NSError* error, NSString* nsUrl,
                                             id<LynxErrorReceiverProtocol> weakErrorReceiver,
