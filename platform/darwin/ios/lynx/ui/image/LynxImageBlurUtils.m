b6199e0b874e1a04bd667c8d67d9568d83c05851
coolkiid <73600915+coolkiid@users.noreply.github.com>
2026-08-24 14:56:30 +0800

    [Infra] Declare Lynx iOS system frameworks (pt.2)
    Lynx/Framework: Accelerate, CoreGraphics, CoreImage, CoreText, QuartzCore lynx/platform/darwin/ios/lynx/BUILD.gn:112:
    - Accelerate: lynx/platform/darwin/ios/lynx/ui/image/LynxImageBlurUtils.m:4, added explicit import for vImage APIs
    - CoreGraphics: lynx/platform/darwin/ios/lynx/ui/image/LynxImageBlurUtils.m:5
    - CoreImage: lynx/platform/darwin/ios/lynx/ui/image/LynxImageBlurUtils.m:6
    - CoreText: lynx/platform/darwin/ios/lynx/shadow_node/text/LynxTextLayoutManager.m:5
    - QuartzCore: lynx/platform/darwin/ios/lynx/renderer/LynxRenderer.mm:11
    
    issue: m-7364497762

--- platform/darwin/ios/lynx/ui/image/LynxImageBlurUtils.m
+++ platform/darwin/ios/lynx/ui/image/LynxImageBlurUtils.m
@@ -1,6 +1,7 @@
 // Copyright 2019 The Lynx Authors. All rights reserved.
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
+#import <Accelerate/Accelerate.h>
 #import <CoreGraphics/CoreGraphics.h>
 #import <CoreImage/CoreImage.h>
 #import <Lynx/LynxImageBlurUtils.h>
