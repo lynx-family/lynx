22130ec5842f4b4d64f2c2af04d05f931307a07b
ci_lynx <ci_lynx@users.noreply.github.com>
2026-09-02 17:21:47 +0800

    [Optimize] Reuse gfx scalar keyframes in Clay animations
    - Replace Clay float and color keyframe storage with shared gfx keyframe primitives.
    - Adapt cloning and interpolation to gfx timing functions while keeping other Clay keyframe types unchanged.
    - Remove unused raw transform compatibility constructors and add coverage for cloned timing functions.

--- clay/gfx/animation/keyframe.cc
+++ clay/gfx/animation/keyframe.cc
@@ -9,138 +9,19 @@
 
 #include "clay/gfx/geometry/filter_operations.h"
 #include "clay/gfx/geometry/transform.h"
-#include "clay/gfx/style/length.h"
 
 namespace clay {
-
-namespace {
-
-Length ToLength(const ClayTransformOP& op, int index) {
-  return Length(op.value[index],
-                op.unit[index] == ClayPlatformLengthUnit::kPercentage
-                    ? LengthUnit::kPercent
-                    : LengthUnit::kNum);
-}
-
-TransformRaw ToTransformRaw(const ClayTransformOP& op) {
-  TransformRaw raw{};
-  raw.type = static_cast<int>(op.type);
-  raw.values[0] = ToLength(op, 0);
-  raw.values[1] = ToLength(op, 1);
-  raw.values[2] = ToLength(op, 2);
-  for (int i = 0; i < 16; ++i) {
-    raw.matrix[i] = op.matrix[i];
-  }
-  return raw;
-}
-
-}  // namespace
 
 Keyframe::Keyframe(float fraction, std::unique_ptr<Interpolator> interpolator)
     : fraction_(fraction), interpolator_(std::move(interpolator)) {}
 
 Keyframe::~Keyframe() = default;
 
-std::unique_ptr<FloatKeyframe> FloatKeyframe::Create(
-    float fraction, float value, std::unique_ptr<Interpolator> interpolator) {
-  return std::unique_ptr<FloatKeyframe>(
-      new FloatKeyframe(fraction, value, std::move(interpolator)));
-}
-
-FloatKeyframe::FloatKeyframe(float fraction, float value,
-                             std::unique_ptr<Interpolator> interpolator)
-    : Keyframe(fraction, std::move(interpolator)), value_(value) {}
-
-FloatKeyframe::~FloatKeyframe() = default;
-
-float FloatKeyframe::Value() const { return value_; }
-
-std::unique_ptr<FloatKeyframe> FloatKeyframe::Clone() const {
-  std::unique_ptr<Interpolator> func;
-  if (GetInterpolator()) {
-    func = GetInterpolator()->Clone();
-  }
-  return FloatKeyframe::Create(GetFraction(), Value(), std::move(func));
-}
-
-#ifndef NDEBUG
-std::string FloatKeyframe::ToString() const {
-  std::ostringstream os;
-  os << "FloatKeyframe: fraction=" << GetFraction() << " value=" << Value();
-  return os.str();
-}
-#endif
-
-std::unique_ptr<ColorKeyframe> ColorKeyframe::Create(
-    float fraction, Color value, std::unique_ptr<Interpolator> interpolator) {
-  return std::unique_ptr<ColorKeyframe>(
-      new ColorKeyframe(fraction, value, std::move(interpolator)));
-}
-
-ColorKeyframe::ColorKeyframe(float fraction, Color value,
-                             std::unique_ptr<Interpolator> interpolator)
-    : Keyframe(fraction, std::move(interpolator)), value_(value) {}
-
-ColorKeyframe::~ColorKeyframe() = default;
-
-Color ColorKeyframe::Value() const { return value_; }
-
-std::unique_ptr<ColorKeyframe> ColorKeyframe::Clone() const {
-  std::unique_ptr<Interpolator> func;
-  if (GetInterpolator()) {
-    func = GetInterpolator()->Clone();
-  }
-  return ColorKeyframe::Create(GetFraction(), Value(), std::move(func));
-}
-
-#ifndef NDEBUG
-std::string ColorKeyframe::ToString() const {
-  std::ostringstream os;
-  os << "ColorKeyframe: fraction=" << GetFraction() << " value=" << std::hex
-     << Value().Value();
-  return os.str();
-}
-#endif
-
-std::unique_ptr<RawTransformKeyframe> RawTransformKeyframe::Create(
-    float fraction, const ClayTransform& transform,
-    std::unique_ptr<Interpolator> interpolator) {
-  return std::unique_ptr<RawTransformKeyframe>(
-      new RawTransformKeyframe(fraction, transform, std::move(interpolator)));
-}
-
-std::unique_ptr<RawTransformKeyframe> RawTransformKeyframe::Create(
-    float fraction, const std::vector<ClayTransformOP>& transform,
-    std::unique_ptr<Interpolator> interpolator) {
-  return std::unique_ptr<RawTransformKeyframe>(
-      new RawTransformKeyframe(fraction, transform, std::move(interpolator)));
-}
-
 std::unique_ptr<RawTransformKeyframe> RawTransformKeyframe::Create(
     float fraction, const std::vector<TransformRaw>& transform,
     std::unique_ptr<Interpolator> interpolator) {
   return std::unique_ptr<RawTransformKeyframe>(
       new RawTransformKeyframe(fraction, transform, std::move(interpolator)));
-}
-
-RawTransformKeyframe::RawTransformKeyframe(
-    float fraction, const ClayTransform& transform,
-    std::unique_ptr<Interpolator> interpolator)
-    : Keyframe(fraction, std::move(interpolator)) {
-  operations_.reserve(transform.size);
-  for (int i = 0; i < transform.size; i++) {
-    operations_.emplace_back(ToTransformRaw(transform.op[i]));
-  }
-}
-
-RawTransformKeyframe::RawTransformKeyframe(
-    float fraction, const std::vector<ClayTransformOP>& transform,
-    std::unique_ptr<Interpolator> interpolator)
-    : Keyframe(fraction, std::move(interpolator)) {
-  operations_.reserve(transform.size());
-  for (const auto& op : transform) {
-    operations_.emplace_back(ToTransformRaw(op));
-  }
 }
 
 RawTransformKeyframe::RawTransformKeyframe(
