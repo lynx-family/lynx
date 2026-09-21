6dc0784fde6135466c21f51262a81e42a547f6d9
gongfeng98 <49113496+gongfeng98@users.noreply.github.com>
2026-09-21 14:25:50 +0800

    Revert "[Feature][Clay] Support scrollBy for scroll-view"
    This reverts commit eae222d40adb164160b3943aae2bbd1af6f68cd6.

--- clay/ui/component/scroll_wrapper.h
+++ clay/ui/component/scroll_wrapper.h
@@ -22,8 +22,6 @@ class ScrollWrapper : public WithTypeInfo<ScrollWrapper, ScrollbarWrapper>,
 
   // UI Method
   void scrollTo(const LynxModuleValues& args);
-  void scrollBy(const LynxModuleValues& args,
-                const LynxUIMethodCallback& callback);
   void autoScroll(const LynxModuleValues& args);
   void getScrollInfo(const LynxModuleValues& args,
                      const LynxUIMethodCallback& callback);
